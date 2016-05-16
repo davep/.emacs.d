fe8c59b4b5195097fecbf052d495c31adfb6247f
Dave Pearson <davep@davep.org>
2016-05-16 09:04:58 +0100

    Make config/lib path names into actual variables.

--- init.el
+++ init.el
@@ -3,15 +3,24 @@
 (require 'cl)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Local config directory support.
+;; Local config/lib directory support.
 
 (defun davep:user-path (path)
   "Given `file', return a path for it in the local config."
   (concat user-emacs-directory path))
 
-(push (davep:user-path "davep/")         load-path)
-(push (davep:user-path "davep/startup/") load-path)
-(push (davep:user-path "davep/lib/")     load-path)
+(defvar davep:local (davep:user-path "davep/")
+  "My local config and code directory.")
+
+(defvar davep:startup (davep:user-path "davep/startup")
+  "My local startup code.")
+
+(defvar davep:lib (davep:user-path "davep/lib")
+  "My local library code.")
+
+(push davep:local   load-path)
+(push davep:startup load-path)
+(push davep:lib     load-path)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ensure custom values go in their own file.
