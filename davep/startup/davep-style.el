47b4e32abbbeaf94a0a630eac93eb15882251df1
Dave Pearson <davep@davep.org>
2016-05-24 14:48:51 +0100

    Don't style when in terminal mode.

--- davep/startup/davep-style.el
+++ davep/startup/davep-style.el
@@ -10,7 +10,8 @@
 	  (lambda ()
 	      (set-frame-size (selected-frame) 90 50)
 	      (set-frame-position (selected-frame) 300 200))))))
-  (funcall (or (cdr (assoc (system-name) styles))
-	       (lambda () nil))))
+  (when window-system
+    (funcall (or (cdr (assoc (system-name) styles))
+		 (lambda () nil)))))
 
 (provide 'davep-style)
