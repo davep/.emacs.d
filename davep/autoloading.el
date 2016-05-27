2a89944846e88deb33753de939e818cbc85cb513
Dave Pearson <davep@davep.org>
2016-05-27 15:15:12 +0100

    Cleaning up the code Ensuring it has fewer warnings. Also removing some Xemacs stuff given I'm highly unlikely to use Xemacs again.

--- davep/autoloading.el
+++ davep/autoloading.el
@@ -1,6 +1,9 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Code for handling the generation of local autoloads.
 
+(eval-when-compile
+  (require 'autoload))
+
 (defvar davep:autoload-file (davep:user-path "davep/autoloads.el")
   "My local autoloads file.")
 
@@ -10,17 +13,14 @@
 (defun update-davep-autoloads-core (file dir)
   "Core function for updating local autoload files."
   (let ((generated-autoload-file file))
-    (cond (gnu-emacs-p
-           (unless (file-exists-p file)
-             (with-temp-buffer
-               (insert 12)
-               (setf (buffer-file-name) file)
-               (save-buffer)))
-           (require 'autoload)
-           (update-autoloads-from-directories dir))
-          (x-emacs-p
-           (update-autoloads-from-directory dir)))
-    (byte-compile-file file)))
+    (unless (file-exists-p file)
+      (with-temp-buffer
+        (insert 12)
+        (setf (buffer-file-name) file)
+        (save-buffer)))
+    (require 'autoload)
+    (update-directory-autoloads dir))
+  (byte-compile-file file))
 
 (defun update-davep-autoloads ()	
   "Update my local autoloads file."
