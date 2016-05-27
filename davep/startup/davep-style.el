db73c24b5ace811a9e308d9127f3b2c5d493556c
Dave Pearson <davep@davep.org>
2016-05-27 15:20:32 +0100

    Untabify stuff.

--- davep/startup/davep-style.el
+++ davep/startup/davep-style.el
@@ -2,16 +2,16 @@
 ;; Visual tweaks.
 (let ((styles
        '(
-	 ("bellerophon" .
-	  (lambda ()
-	      (set-frame-size (selected-frame) 90 50)
-	      (set-frame-position (selected-frame) 230 60)))
-	 ("ariel" .
-	  (lambda ()
-	      (set-frame-size (selected-frame) 90 50)
-	      (set-frame-position (selected-frame) 300 200))))))
+         ("bellerophon" .
+          (lambda ()
+              (set-frame-size (selected-frame) 90 50)
+              (set-frame-position (selected-frame) 230 60)))
+         ("ariel" .
+          (lambda ()
+              (set-frame-size (selected-frame) 90 50)
+              (set-frame-position (selected-frame) 300 200))))))
   (when window-system
     (funcall (or (cdr (assoc (car (split-string (downcase (system-name)) "\\.")) styles))
-		 (lambda () nil)))))
+                 (lambda () nil)))))
 
 (provide 'davep-style)
