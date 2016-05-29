3a2f3a750371a380a62855bf31ee86fe545d1b62
Dave Pearson <davep@davep.org>
2016-05-29 19:54:03 +0100

    Show images when we visit them

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-image-file-mode t)
  '(blink-cursor-mode nil)
  '(case-fold-search t)
  '(column-number-mode t)
