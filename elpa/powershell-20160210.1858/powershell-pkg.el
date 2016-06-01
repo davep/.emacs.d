b3ff970255f7b3f29e113ca1d70455a83a44771f
Dave Pearson <davep@davep.org>
2016-06-01 10:51:01 +0100

    Add powershell mode.

---
+++ elpa/powershell-20160210.1858/powershell-pkg.el
@@ -0,0 +1,1 @@
+(define-package "powershell" "20160210.1858" "Mode for editing Powershell scripts" '((emacs "24")) :url "http://github.com/jschaf/powershell.el" :keywords '("powershell" "languages"))
