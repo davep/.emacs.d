8e54f34b1aa85ccc83220eee268cd1a6c664bff4
Dave Pearson <davep@davep.org>
2016-09-10 14:15:23 +0100

    Add highlight-chars

---
+++ elpa/highlight-chars-20160729.2320/highlight-chars-pkg.el
@@ -0,0 +1,1 @@
+(define-package "highlight-chars" "20160729.2320" "Highlight specified sets of characters, including whitespace." 'nil :url "http://www.emacswiki.org/highlight-chars.el" :keywords '("highlight" "whitespace" "characters" "unicode"))
