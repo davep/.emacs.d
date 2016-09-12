b7ddb0d80511a9e6630ed8785ee63d2261565fae
Dave Pearson <davep@davep.org>
2016-09-12 12:57:44 +0100

    Add long-term-emacs back into the mix.

--- davep/startup/davep-misc.el
+++ davep/startup/davep-misc.el
@@ -37,4 +37,15 @@
 (when (and davep:unixp (file-exists-p "~/bin"))
   (push "~/bin/" exec-path))
 
+(defun long-term-emacs ()
+  "Turn this emacs sesssion into a long term emacs.
+
+This involves disabling C-x C-c and also calling `server-start'."
+  (interactive)
+  (server-start)
+  (define-key global-map [(control x) (control c)]
+    #'(lambda ()
+        (interactive)
+        (message "C-x C-c is disabled"))))
+
 (provide 'davep-misc)
