6725f0c0cf41046d0ce464d992c4a6a9038da936
Dave Pearson <davep@davep.org>
2017-03-23 09:51:44 +0000

    Remove my old parenface

--- elpa/parenface-1.1/parenface-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "parenface" "1.1" "Provide a face for parens in lisp modes." 'nil :authors '(("Dave Pearson" . "davep@davep.org")) :maintainer '("Dave Pearson" . "davep@davep.org") :url "https://github.com/davep/parenface.el")
