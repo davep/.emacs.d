129edce74f60b409673999edc1cba5652efa3cab
Dave Pearson <davep@davep.org>
2017-03-23 14:04:44 +0000

    Install webinfo from a package these days

---
+++ elpa/webinfo-1.1/webinfo-autoloads.el
@@ -0,0 +1,25 @@
+;;; webinfo-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "webinfo" "webinfo.el" (22739 54481 0 0))
+;;; Generated autoloads from webinfo.el
+
+(autoload 'webinfo "webinfo" "\
+Get some information about HTTP server at HOST on PORT.
+
+\(fn HOST PORT)" t nil)
+
+;;;***
+
+;;;### (autoloads nil nil ("webinfo-pkg.el") (22739 54351 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; webinfo-autoloads.el ends here
