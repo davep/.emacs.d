5046e4efd915731f108c739639258b299df33911
Dave Pearson <davep@davep.org>
2017-03-25 10:47:35 +0000

    Remove old uptimes

--- elpa/uptimes-2.5/uptimes-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "uptimes" "2.5" "Track and display emacs session uptimes." 'nil :authors '(("Dave Pearson" . "davep@davep.org")) :maintainer '("Dave Pearson" . "davep@davep.org") :url "https://github.com/davep/uptimes.el")
