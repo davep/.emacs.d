3d7b18425b5cb2e3f8c3bdbfcfc0badcb26db921
Dave Pearson <davep@davep.org>
2017-03-25 10:46:12 +0000

    Update uptimes.el

---
+++ elpa/uptimes-3.0/uptimes-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "uptimes" "3.0" "Track and display emacs session uptimes." '((cl-lib "0.5")) :authors '(("Dave Pearson" . "davep@davep.org")) :maintainer '("Dave Pearson" . "davep@davep.org") :url "https://github.com/davep/uptimes.el")
