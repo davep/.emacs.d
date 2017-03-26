f7380d518bb27cb7ad3bfb5ba0123c65d379741a
Dave Pearson <davep@davep.org>
2017-03-26 19:57:29 +0100

    Resync emacs custom values I just realised that there's a flaw in how I manage custom values via GitHub. I'm making the changes in long-running copies of Emacs and they don't always have the same state. From now on I need to ensure that I kill the long-running one in the Macbook, at least, to ensure things work.
    Meanwile... this is supposed to add elisp.org (the Org file I'm using for some
    elisp change tracking) to the Org agenda.

--- custom.el
+++ custom.el
@@ -6,6 +6,11 @@
  '(auto-image-file-mode t)
  '(blink-cursor-mode nil)
  '(case-fold-search t)
+ '(clean-buffer-list-delay-general 1)
+ '(clean-buffer-list-delay-special 1)
+ '(clean-buffer-list-kill-buffer-names
+   (quote
+    ("*Help*" "*Apropos*" "*Buffer List*" "*Compile-Log*" "*info*" "*vc*" "*vc-diff*" "*diff*" "*uptimes*" "*markdown-output*" "*Checkdoc Status*" "*Compile-Log*")))
  '(column-number-mode t)
  '(cursor-in-non-selected-windows nil)
  '(diff-switches "-u")
@@ -42,15 +47,44 @@
  '(mouse-avoidance-mode (quote jump) nil (avoid))
  '(ns-alternate-modifier (quote super))
  '(ns-command-modifier (quote meta))
- '(org-directory "~/Dropbox/Sync/Org")
- '(org-log-done (quote time))
+ '(org-agenda-files (quote ("~/Dropbox/Sync/Org/elisp.org")))
  '(package-archives
    (quote
     (("gnu" . "http://elpa.gnu.org/packages/")
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (binclock thinks uptimes w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
+    (nukneval unbind fscroll webinfo obfusurl paren-face parenface binclock uptimes thinks package-lint w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
+ '(safe-local-variable-values
+   (quote
+    ((eval when
+           (and
+            (buffer-file-name)
+            (file-regular-p
+             (buffer-file-name))
+            (string-match-p "^[^.]"
+                            (buffer-file-name)))
+           (unless
+               (featurep
+                (quote package-build))
+             (let
+                 ((load-path
+                   (cons "../package-build" load-path)))
+               (require
+                (quote package-build))))
+           (package-build-minor-mode)
+           (set
+            (make-local-variable
+             (quote package-build-working-dir))
+            (expand-file-name "../working/"))
+           (set
+            (make-local-variable
+             (quote package-build-archive-dir))
+            (expand-file-name "../packages/"))
+           (set
+            (make-local-variable
+             (quote package-build-recipes-dir))
+            default-directory)))))
  '(sass-indent-offset 4)
  '(savehist-mode t)
  '(scroll-error-top-bottom t)
@@ -66,4 +100,4 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(parenthesis ((t (:inherit shadow :foreground "gray63")))))
