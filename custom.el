f18c4c157c9703b80adcfd05ac4c474fec56fbbb
Dave Pearson <davep@davep.org>
2017-03-27 15:54:09 +0100

    Install protocols.el via package management

--- custom.el
+++ custom.el
@@ -59,7 +59,7 @@
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (nukneval unbind fscroll webinfo obfusurl paren-face parenface binclock uptimes thinks package-lint w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
+    (protocols nukneval unbind fscroll webinfo obfusurl paren-face parenface binclock uptimes thinks package-lint w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
  '(safe-local-variable-values
    (quote
     ((eval when
---
+++ elpa/protocols-1.5/protocols-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "protocols" "1.5" "Protocol database access functions." '((cl-lib "0.5")) :authors '(("Dave Pearson" . "davep@davep.org")) :maintainer '("Dave Pearson" . "davep@davep.org") :url "https://github.com/davep/protocols.el")
