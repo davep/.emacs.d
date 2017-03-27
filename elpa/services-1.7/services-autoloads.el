0d77688d0b69464ed890fd89fce4d513bb02501f
Dave Pearson <davep@davep.org>
2017-03-27 16:35:17 +0100

    Install services.el via package management

---
+++ elpa/services-1.7/services-autoloads.el
@@ -0,0 +1,26 @@
+;;; services-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "services" "services.el" (22745 12535 0 0))
+;;; Generated autoloads from services.el
+
+(autoload 'services-lookup "services" "\
+Find a service given by SEARCH and PROTOCOL and display its details.
+
+\(fn SEARCH PROTOCOL)" t nil)
+
+(autoload 'services-clear-cache "services" "\
+Clear the services \"cache\".
+
+\(fn)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; services-autoloads.el ends here
