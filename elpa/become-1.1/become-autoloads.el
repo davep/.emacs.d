a1a6bddb96e4d90e4593af96ddbcc4dc04f06164
Dave Pearson <davep@davep.org>
2017-03-28 14:07:37 +0100

    Updated become.el

--- elpa/become-1.1/become-autoloads.el
+++ elpa/become-1.1/become-autoloads.el
@@ -3,7 +3,7 @@
 ;;; Code:
 (add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
 
-;;;### (autoloads nil "become" "become.el" (22746 7518 0 0))
+;;;### (autoloads nil "become" "become.el" (22746 24524 0 0))
 ;;; Generated autoloads from become.el
 
 (autoload 'become-dos-buffer "become" "\
@@ -18,6 +18,29 @@ Turn the current buffer into a Unix buffer.
 
 (autoload 'become-mac-buffer "become" "\
 Turn the current buffer into a Mac buffer.
+
+\(fn)" t nil)
+
+(autoload 'become-undosly "become" "\
+Strip current buffer of DOS line end markers.
+
+\(fn)" t nil)
+
+(autoload 'become-freshly-indented "become" "\
+Apply indentation to whole buffer.
+
+\(fn)" t nil)
+
+(autoload 'become-freshly-indented-no-tabs "become" "\
+Apply indentation to whole buffer and then untabify.
+
+\(fn)" t nil)
+
+(autoload 'become-free-of-trailing-whitespace "become" "\
+Remove all trailing whitespace from all lines in the current buffer.
+
+Note that this function makes a point of not stripping the trailing space
+from a signature seperator line.
 
 \(fn)" t nil)
 
