f5db6fe8e2796880bf384904c95527fcea16e4e9
Dave Pearson <davep@davep.org>
2017-03-28 14:48:31 +0100

    Installing csrclr via package management now

--- custom.el
+++ custom.el
@@ -59,7 +59,7 @@
      ("melpa" . "https://melpa.org/packages/"))))
  '(package-selected-packages
    (quote
-    (moving insert become itch services protocols nukneval unbind fscroll webinfo obfusurl paren-face parenface binclock uptimes thinks package-lint w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
+    (csrclr moving insert become itch services protocols nukneval unbind fscroll webinfo obfusurl paren-face parenface binclock uptimes thinks package-lint w3m slime sass-mode restclient powershell markdown-mode hyde highlight-chars emojify dictionary csharp-mode browse-kill-ring boxquote auto-compile)))
  '(safe-local-variable-values
    (quote
     ((eval when
---
+++ elpa/csrclr-1.3/csrclr-autoloads.el
@@ -0,0 +1,15 @@
+;;; csrclr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil nil ("csrclr.el") (22746 27015 0 0))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; csrclr-autoloads.el ends here
