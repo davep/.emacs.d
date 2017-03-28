bcde4fa35a45061bcefbcc182bcdbf2b2a008737
Dave Pearson <davep@davep.org>
2017-03-28 16:43:59 +0100

    Revive handyurl.el

---
+++ .handyurls.el
@@ -0,0 +1,2 @@
+(( "The GNU Project" . "http://www.gnu.org/")
+ ( "The FSF"         . "http://www.fsf.org/"))
---
+++ elpa/handyurl-2.0/handyurl-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "handyurl" "2.0" "Pop up a list of URLs and select for pasting." '((cl-lib "0.5")) :authors '(("Dave Pearson" . "davep@davep.org")) :maintainer '("Dave Pearson" . "davep@davep.org") :url "https://github.com/davep/handyurl.el")
