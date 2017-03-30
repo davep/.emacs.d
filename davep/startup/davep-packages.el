c6d2268138927111f93cc9fb0b821a72e0ee6d04
Dave Pearson <davep@davep.org>
2017-03-30 12:23:36 +0100

    Start moving key bindings out of davep-keys and into use-package Hopefully, by the time I'm done, I'll have moved most things about such that davep-keys either isn't needed any more, or it's a more tidy collection of keys that don't relate to "third party" packages but, insted, relate just to standard emacs packages.

--- davep/startup/davep-packages.el
+++ davep/startup/davep-packages.el
@@ -8,20 +8,41 @@
 (use-package binclock :ensure t)
 (use-package csrclr   :ensure t)
 (use-package fscroll  :ensure t)
-(use-package insert   :ensure t)
-(use-package itch     :ensure t)
-(use-package moving   :ensure t)
+(use-package insert   :ensure t
+             :bind
+             ("C-RET"   . insert-line-split-keeping-fill-prefix)
+             ("C-c RET" . insert-line-split-keeping-fill-prefix)
+             ("C-c f"   . insert-filename))
+(use-package itch     :ensure t
+             :bind
+             ("M-s" . itch-scratch-buffer))
+(use-package moving   :ensure t
+             :bind
+             ([home] . moving-home) ([end] . moving-end))
 (use-package nukneval :ensure t)
 (use-package unbind   :ensure t)
 (use-package webinfo  :ensure t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Packages I've written, that are in melpa.
-(use-package boxquote  :ensure t)
+(use-package boxquote  :ensure t
+             :bind
+             ("C-c i"     . boxquote-insert-file)
+             ("C-c M-w"   . boxquote-kill-ring-save)
+             ("C-c y"     . boxquote-yank)
+             ("C-c b"     . boxquote-region)
+             ("C-c C-b"   . boxquote-title)
+             ("C-c C-h f" . boxquote-describe-function)
+             ("C-c C-h v" . boxquote-describe-variable)
+             ("C-c C-h k" . boxquote-describe-key)
+             ("C-c !"     . boxquote-shell-command)
+             ("C-c C-h w" . boxquote-where-is))
 (use-package obfusurl  :ensure t)
 (use-package protocols :ensure t)
 (use-package services  :ensure t)
-(use-package thinks    :ensure t)
-(use-package uptimes   :ensure t)
+(use-package thinks    :ensure t :bind ("C-c C-t" . thinks-maybe-region))
+(use-package uptimes   :ensure t
+             :bind
+             ([f11] . uptimes-current) ("C-c t" . uptimes))
 
 (provide 'davep-packages)
