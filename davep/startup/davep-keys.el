f4f7e530a8418d0490c77938548de4634525b466
Dave Pearson <davep@davep.org>
2017-03-30 12:48:02 +0100

    Tidy up davep-keys to use bind-key

--- davep/startup/davep-keys.el
+++ davep/startup/davep-keys.el
@@ -1,46 +1,39 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; personal keyboard bindings
+;; Personal keyboard bindings
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-(define-key global-map [(control x)
-                        (control b)]     #'ibuffer)
-(define-key global-map [(meta f6)]       #'ibuffer)
-(define-key global-map [(control f6)]    #'list-processes)
-(define-key global-map [f6]              #'bury-buffer)
-(define-key global-map [f9]              #'compile)
-(define-key global-map [(meta g)]        #'goto-line)
-(define-key global-map [(alt l)]         #'ispell-word)
-(define-key global-map [(meta left)]     #'backward-sexp)
-(define-key global-map [(meta right)]    #'forward-sexp)
-(define-key global-map [(meta p)]        #'(lambda ()
-                                             (interactive)
-                                             (other-window -1)))
-(define-key global-map [(meta n)]        #'other-window)
-(define-key global-map [(control c) (e)] #'eshell)
-(define-key global-map [(control c) (k)] #'browse-kill-ring)
-(define-key global-map [(control c) (o)] #'(lambda ()
-                                             (interactive)
-                                             (require 'org)
-                                             (find-file (concat org-directory "/inbox.org"))))
-(define-key global-map [(control c)
-                        (control o)]     #'(lambda ()
-                                             (interactive)
-                                             (require 'org)
-                                             (find-file org-directory)))
-(define-key global-map [(control c) (r)] #'comment-region)
-(define-key global-map [(control f11)]   #'clean-buffer-list)
-(define-key global-map [delete]          #'delete-char)
-(define-key global-map [(control
-                         backspace)]     #'backward-kill-word)
-(define-key global-map [(meta
-                         backspace)]     #'undo)
-(define-key global-map [(control z)]     #'undo)
-(define-key global-map [(control c)
-                        (control f)]     #'view-file)
-(define-key global-map [(control c) (a)] #'org-agenda)
+(require 'bind-key)
+
+;; General bindinds
+(bind-key "C-x C-b"             #'ibuffer)
+(bind-key [(meta f6)]           #'ibuffer)
+(bind-key [(control f6)]        #'list-processes)
+(bind-key [f6]                  #'bury-buffer)
+(bind-key [f9]                  #'compile)
+(bind-key "M-g"                 #'goto-line)
+(bind-key [(meta left)]         #'backward-sexp)
+(bind-key [(meta right)]        #'forward-sexp)
+(bind-key "C-c e"               #'eshell)
+(bind-key [(control f11)]       #'clean-buffer-list)
+(bind-key "C-c r"               #'comment-region)
+(bind-key "C-c C-f"             #'view-file)
+(bind-key "M-p"                 #'(lambda () (interactive) (other-window -1)))
+(bind-key "M-n"                 #'other-window)
+(bind-key "C-c k"               #'browse-kill-ring)
+(bind-key [delete]              #'delete-char)
+(bind-key [(control backspace)] #'backward-kill-word)
+(bind-key [(meta backspace)]    #'undo)
+(bind-key "C-z"                 #'undo)
+(bind-key "C-c o"               #'(lambda () (interactive) (require 'org) (find-file (concat org-directory "/inbox.org"))))
+(bind-key "C-c C-o"             #'(lambda () (interactive) (require 'org) (find-file org-directory)))
+(bind-key "C-c a"               #'org-agenda)
+
+;; Things specific to macOS.
 (when davep:osx-window-p
   (define-key global-map [(super tab)] #'completion-at-point))
+
+;; Things specific to Windows.
 (when davep:win32p
-  (setq w32-lwindow-modifier 'meta))
+  (set 'w32-lwindow-modifier 'meta))
 
 (provide 'davep-keys)
