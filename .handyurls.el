daa2d0b945c5401ea8294c6c354f3d0f1e0c6375
Dave Pearson <davep@davep.org>
2017-04-02 09:47:23 +0100

    Add a couple more URLs to my handy URL list I should really start using quickurl again.

--- .handyurls.el
+++ .handyurls.el
@@ -1,2 +1,4 @@
 (( "The GNU Project" . "http://www.gnu.org/")
- ( "The FSF"         . "http://www.fsf.org/"))
+ ( "The FSF"         . "http://www.fsf.org/")
+ ( "davep.org"       . "http://www.davep.org/")
+ ( "My GitHub"       . "https://github.com/davep"))
