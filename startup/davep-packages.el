4e2a5c1d6f3b63485a55994b72e2fc7f3994d6a4
Dave Pearson <davep@davep.org>
2017-04-04 22:38:56 +0100

    Add mc/dwim to the mix

--- startup/davep-packages.el
+++ startup/davep-packages.el
@@ -166,8 +166,9 @@
 (use-package multiple-cursors
     :ensure t
     :bind
-    ("C-." . mc/mark-next-like-this)
-    ("C-," . mc/mark-previous-like-this)
-    ("C->" . mc/mark-all-like-this))
+    ("C-."   . mc/mark-next-like-this)
+    ("C-,"   . mc/mark-previous-like-this)
+    ("C->"   . mc/mark-all-like-this)
+    ("C-c ," . mc/mark-all-like-this-dwim))
 
 (provide 'davep-packages)
