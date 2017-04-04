01e6ee3e4b13941cffaf764f00f8da407f3b5c25
Dave Pearson <davep@davep.org>
2017-04-04 22:41:38 +0100

    Got intended binding for mc/dwim wrong; fix

--- startup/davep-packages.el
+++ startup/davep-packages.el
@@ -169,6 +169,6 @@
     ("C-."   . mc/mark-next-like-this)
     ("C-,"   . mc/mark-previous-like-this)
     ("C->"   . mc/mark-all-like-this)
-    ("C-c ," . mc/mark-all-like-this-dwim))
+    ("C-c ." . mc/mark-all-like-this-dwim))
 
 (provide 'davep-packages)
