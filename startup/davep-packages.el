265dd6ffcf76f40400143a57d3a4c469bacdca73
Dave Pearson <davep@davep.org>
2017-04-04 22:19:31 +0100

    Pick what I think are better keys for multiple-cursors

--- startup/davep-packages.el
+++ startup/davep-packages.el
@@ -166,8 +166,8 @@
 (use-package multiple-cursors
     :ensure t
     :bind
-    ("C->"     . mc/mark-next-like-this)
-    ("C-<"     . mc/mark-previous-like-this)
-    ("C-c C-<" . mc/mark-all-like-this))
+    ("C-." . mc/mark-next-like-this)
+    ("C-," . mc/mark-previous-like-this)
+    ("C->" . mc/mark-all-like-this))
 
 (provide 'davep-packages)
