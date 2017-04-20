b9f9f3b6fff246e919160d6f3cb64c2646a3a0a9
Dave Pearson <davep@davep.org>
2017-04-20 12:03:11 +0100

    Reindentation

--- startup/davep-style.el
+++ startup/davep-style.el
@@ -4,12 +4,12 @@
        '(
          ("bellerophon" .
           (lambda ()
-              (set-frame-size (selected-frame) 90 50)
-              (set-frame-position (selected-frame) 230 60)))
+            (set-frame-size (selected-frame) 90 50)
+            (set-frame-position (selected-frame) 230 60)))
          ("ariel" .
           (lambda ()
-              (set-frame-size (selected-frame) 90 50)
-              (set-frame-position (selected-frame) 300 200))))))
+            (set-frame-size (selected-frame) 90 50)
+            (set-frame-position (selected-frame) 300 200))))))
   (when window-system
     (funcall (or (cdr (assoc (car (split-string (downcase (system-name)) "\\.")) styles))
                  (lambda () nil)))))
