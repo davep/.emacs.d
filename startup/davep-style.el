119e4a6200afd37a7495b93fbe2496f8028f8d54
Dave Pearson <davep@davep.org>
2017-04-26 13:42:26 +0100

    Use a powerline-friendly font in grapical Mac emacs Also tweak the initial window postion on my iMac

--- startup/davep-style.el
+++ startup/davep-style.el
@@ -9,7 +9,7 @@
          ("ariel" .
           (lambda ()
             (set-frame-size (selected-frame) 90 50)
-            (set-frame-position (selected-frame) 300 200))))))
+            (set-frame-position (selected-frame) 300 80))))))
   (when window-system
     (funcall (or (cdr (assoc (car (split-string (downcase (system-name)) "\\.")) styles))
                  (lambda () nil)))))
@@ -33,5 +33,12 @@
 ;; Ensure line numbers are off in the terminal.
 (unless window-system
   (global-linum-mode -1))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Use a powerline-friendly font when running graphically on a Mac.
+(when davep:macOS-window-p
+  (let ((font "DejaVu Sans Mono for Powerline"))
+    (set-face-attribute 'default nil :font font)
+    (set-frame-font font nil t)))
 
 (provide 'davep-style)
