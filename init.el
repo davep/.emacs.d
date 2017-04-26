84c7f9f31de9d09627b550108d94594a4e90d462
Dave Pearson <davep@davep.org>
2017-04-26 09:26:00 +0100

    Move some of the "tweaks" into their own file

--- init.el
+++ init.el
@@ -24,32 +24,11 @@
 (require 'davep-env-tests)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; On the Mac local bin doesn't seem to be in the path if I run from
-;; the dock. Fix this.
-(let ((local "/usr/local/bin"))
-  (when (and davep:macOS-p (not (member local exec-path)))
-    (push local exec-path)))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; If we're on a Unix of some sort, add a personal bin (if it's there).
-(when (and davep:unixp (file-exists-p "~/bin"))
-  (push "~/bin/" exec-path))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Load various startup things.
 (unless noninteractive
+  (require 'davep-tweaks)
   (require 'davep-packages)
   (require 'davep-keys)
   (require 'davep-style))
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; Enable some disabled commands
-(mapc #'(lambda (command)
-          (put command 'disabled nil))
-      '(downcase-region
-        erase-buffer
-        narrow-to-page
-        narrow-to-region
-        upcase-region))
 
 ;;; init.el ends here
---
+++ startup/davep-tweaks.el
@@ -0,0 +1,23 @@
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; On the Mac local bin doesn't seem to be in the path if I run from
+;; the dock. Fix this.
+(let ((local "/usr/local/bin"))
+  (when (and davep:macOS-p (not (member local exec-path)))
+    (push local exec-path)))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; If we're on a Unix of some sort, add a personal bin (if it's there).
+(when (and davep:unixp (file-exists-p "~/bin"))
+  (push "~/bin/" exec-path))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Enable some disabled commands
+(mapc #'(lambda (command)
+          (put command 'disabled nil))
+      '(downcase-region
+        erase-buffer
+        narrow-to-page
+        narrow-to-region
+        upcase-region))
+
+(provide 'davep-tweaks)
