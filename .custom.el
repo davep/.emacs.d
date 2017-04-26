94b25c37596add3826501c65ebe041119293f78c
Dave Pearson <davep@davep.org>
2017-04-26 11:56:24 +0100

    Improve the shell prompt Sort the /Users/davep vs ~ problem I've had since I set my own prompt, and also change the colour to something a little more subtle than bright red.

--- .custom.el
+++ .custom.el
@@ -41,7 +41,8 @@
      (concat
       (user-login-name)
       ":"
-      (eshell/pwd)
+      (abbreviate-file-name
+       (eshell/pwd))
       (if davep:rootp "#" "$")
       " ")))
  '(eshell-prompt-regexp "^[^#$\\n]*[#$] ")
@@ -133,4 +134,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eshell-prompt ((t (:foreground "indian red" :weight bold))))
  '(parenthesis ((t (:inherit shadow :foreground "gray63")))))
