02c28498ad346b7ac85a45ac6e4de3deb8012fa0
Dave Pearson <davep@davep.org>
2017-05-03 10:14:04 +0100

    Drop handyurl in favour of quickurl
    It's why I wrote it in the first place. While it makes sense that I've
    tidied up handyurl.el, it's not really that useful to me these days (it's
    questionable that quickurl.el is, but it is the better of the two).

--- .handyurls.el
+++
@@ -1,4 +0,0 @@
-(( "The GNU Project" . "http://www.gnu.org/")
- ( "The FSF"         . "http://www.fsf.org/")
- ( "davep.org"       . "http://www.davep.org/")
- ( "My GitHub"       . "https://github.com/davep"))
--- .quickurls.el
+++ .quickurls.el
@@ -1,3 +1,6 @@
 ;; -*- lisp -*-
 
-(("davep" "http://www.davep.org/" "My homepage"))
+(("davep" "http://www.davep.org/" "My homepage")
+ ("fsf"   "http://www.fsf.org/"   "The FSF")
+ ("gnu"   "http://gnu.org/"       "The GNU Project")
+ ("github" "https://github.com/davep" "My GitHub"))
