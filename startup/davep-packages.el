f823310c109a8c048f739a766f9e7a9d9acccba3
Dave Pearson <davep@davep.org>
2017-05-04 10:17:58 +0100

    Keep checkdoc happy

--- startup/davep-packages.el
+++ startup/davep-packages.el
@@ -1,8 +1,19 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; davep-packages.el --- Package loading
+
+;;; Commentary:
+;;
+;; davep-packages.el is reponsible for loading the packages I want. This
+;; makes very heavy use of use-package and is designed to help bootstrap a
+;; fresh Emacs installation.
+;;
+;; http://blog.davep.org/2017/04/01/another_revamp_of_my_emacs_config.html
+;; contains a bit of background.
+
+;;; Code:
+
 ;; Ensure use-package is available.
 (require 'use-package)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Given I have my own package archive, ensure I can manage that too.
 (require 'package-x)
 
@@ -265,3 +276,5 @@
 (add-hook 'emacs-lisp-mode-hook #'setup-compile)
 
 (provide 'davep-packages)
+
+;;; davep-packages.el ends here
