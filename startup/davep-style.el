522c4dec264b8a77ccd034c42ba853c78b7c79ad
Dave Pearson <davep@davep.org>
2017-05-04 10:14:41 +0100

    Keep checkdoc happy

--- startup/davep-style.el
+++ startup/davep-style.el
@@ -1,4 +1,12 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; davep-style.el --- Style Emacs to my taste
+
+;;; Commentary:
+;;
+;; davep-style.el contains code that styles Emacs to my taste. It includes
+;; things like the default window size/position, theme choices, etc.
+
+;;; Code:
+
 ;; Window position and size.
 (let ((styles
        '(
@@ -14,24 +22,22 @@
     (funcall (or (cdr (assoc (car (split-string (downcase (system-name)) "\\.")) styles))
                  (lambda () nil)))))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Window titles.
 (let ((format (concat invocation-name "@" (system-name) " - [%b]")))
   (setq frame-title-format format
         icon-title-format  format))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Really don't need menus in a terminal.
 (unless window-system
   (menu-bar-mode -1))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Default colour scheme.
 (load-theme (if window-system 'adwaita 'deeper-blue) t)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ensure line numbers are off in the terminal.
 (unless window-system
   (global-linum-mode -1))
 
 (provide 'davep-style)
+
+;;; davep-style.el ends here
