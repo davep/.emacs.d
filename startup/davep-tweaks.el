8d564c6653171e246eb24be8bc45a51e926a87b9
Dave Pearson <davep@davep.org>
2017-05-04 10:10:45 +0100

    Make checkdoc happy

--- startup/davep-tweaks.el
+++ startup/davep-tweaks.el
@@ -1,17 +1,24 @@
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; On the Mac local bin doesn't seem to be in the path if I run from
-;; the dock. Fix this.
+;;; davep-tweaks.el --- Miscellaneous "tweaks" to my emacs environment
+
+;;; Commentary:
+;;
+;; davep-tweaks.el contains miscellaneous tweaks to the Emacs environment.
+;; These are the sorts of changes that don't directly relate to packages,
+;; etc, and aren't about how Emacs looks.
+
+;;; Code:
+
+;; On the Mac local bin doesn't seem to be in the path if I run from the
+;; dock. Fix this.
 (let ((local "/usr/local/bin"))
   (when (and davep:macOS-p (not (member local exec-path)))
     (push local exec-path)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; If we're on a Unix of some sort, add a personal bin (if it's there).
 (let ((bin "~/bin"))
   (when (and davep:unixp (file-exists-p bin))
     (push bin exec-path)))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Enable some disabled commands
 (mapc (lambda (command)
         (put command 'disabled nil))
@@ -22,3 +29,5 @@
         upcase-region))
 
 (provide 'davep-tweaks)
+
+;;; davep-tweaks.el ends here
