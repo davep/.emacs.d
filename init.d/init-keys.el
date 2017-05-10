c325101fb15b0f130c7762a82e7e5fb8361487b4
Dave Pearson <davep@davep.org>
2017-05-10 11:37:53 +0100

    Better group keyboard bindings

--- init.d/init-keys.el
+++ init.d/init-keys.el
@@ -9,24 +9,28 @@
 (require 'is-a)
 (require 'bind-key)
 
-;; General bindinds
-(bind-key [(control f6)]        #'list-processes)
-(bind-key [f6]                  #'bury-buffer)
-(bind-key [f9]                  #'compile)
-(bind-key "M-g"                 #'goto-line)
-(bind-key [(meta left)]         #'backward-sexp)
-(bind-key [(meta right)]        #'forward-sexp)
-(bind-key "C-c e"               #'eshell)
-(bind-key [(control f11)]       #'clean-buffer-list)
+;; Movement
+(bind-key "M-g"          #'goto-line)
+(bind-key [(meta left)]  #'backward-sexp)
+(bind-key [(meta right)] #'forward-sexp)
+(bind-key "M-p"          (lambda () (interactive) (other-window -1)))
+(bind-key "M-n"          #'other-window)
+
+;; Editing
 (bind-key "C-c ;"               #'comment-or-uncomment-region)
-(bind-key "C-c C-f"             #'view-file)
-(bind-key "M-p"                 (lambda () (interactive) (other-window -1)))
-(bind-key "M-n"                 #'other-window)
 (bind-key [delete]              #'delete-char)
 (bind-key [(control backspace)] #'backward-kill-word)
 (bind-key [(meta backspace)]    #'undo)
 (bind-key "C-z"                 #'undo)
-(bind-key "s-r"                 #'ielm)
+
+;; Tools and "apps" and things.
+(bind-key [(control f6)]  #'list-processes)
+(bind-key [f6]            #'bury-buffer)
+(bind-key [f9]            #'compile)
+(bind-key "C-c e"         #'eshell)
+(bind-key [(control f11)] #'clean-buffer-list)
+(bind-key "C-c C-f"       #'view-file)
+(bind-key "s-r"           #'ielm)
 
 ;; Things specific to macOS.
 (when is-a-macOS-window-p
