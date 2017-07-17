058f6c9997197989ed88667d2f69c9ed2d6ab275
Dave Pearson <davep@davep.org>
2017-07-17 10:41:45 +0100

    Remove package system barf
    Didn't know it did this. I might have to reconsider how I go about this.

--- init.el
+++ init.el
@@ -1,12 +1,5 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;; Ensure custom values go in their own file.
-
-;; Added by Package.el.  This must come before configurations of
-;; installed packages.  Don't delete this line.  If you don't want it,
-;; just comment it out by adding a semicolon to the start of the line.
-;; You may delete these explanatory comments.
-(package-initialize)
-
 (load (setq custom-file (concat user-emacs-directory ".custom.el")) t)
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
