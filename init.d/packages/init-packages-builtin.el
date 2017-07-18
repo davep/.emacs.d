887bf61fb5d46d0121d4f7f6bdc94e6631720430
Dave Pearson <davep@davep.org>
2017-07-18 11:51:54 +0100

    Don't call setq twice when once will do

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -99,8 +99,8 @@
   :config
   (add-hook 'message-mode-hook
             (lambda ()
-              (setq smartsig-set-signature #'msig-set)
-              (setq smartsig-abbrev-table  'message-mode-abbrev-table)
+              (setq smartsig-set-signature #'msig-set
+                    smartsig-abbrev-table  'message-mode-abbrev-table)
               (smartsig-clear)
               (smartsig-add "emacs"   "~/.sigs/emacs"   "emacs" "xemacs" "elisp" "gnu" "lbdb" "uptimes" "quickurl" "smartsig" "boxquote")
               (smartsig-add "sawfish" "~/.sigs/sawfish" "sawfish" "sawmill" "librep" "rep" "gnome"))))
