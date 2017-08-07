1db3c4c528a675e63dd79a1e576bd77a0ed50d26
Dave Pearson <davep@davep.org>
2017-08-07 16:20:41 +0100

    Rethink the bindings for boxquote.el
    I actually don't use it much these days (it was most useful back in the days
    of usenet - so getting on for 20 years ago) but it's nice to keep around.

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -18,16 +18,16 @@
 (use-package boxquote
   :ensure t
   :bind
-  ("C-c i"     . boxquote-insert-file)
-  ("C-c M-w"   . boxquote-kill-ring-save)
-  ("C-c y"     . boxquote-yank)
-  ("C-c b"     . boxquote-region)
-  ("C-c C-b"   . boxquote-title)
-  ("C-c C-h f" . boxquote-describe-function)
-  ("C-c C-h v" . boxquote-describe-variable)
-  ("C-c C-h k" . boxquote-describe-key)
-  ("C-c !"     . boxquote-shell-command)
-  ("C-c C-h w" . boxquote-where-is))
+  ("<f12> b i"   . boxquote-insert-file)
+  ("<f12> b M-w" . boxquote-kill-ring-save)
+  ("<f12> b y"   . boxquote-yank)
+  ("<f12> b b"   . boxquote-region)
+  ("<f12> b t"   . boxquote-title)
+  ("<f12> b h f" . boxquote-describe-function)
+  ("<f12> b h v" . boxquote-describe-variable)
+  ("<f12> b h k" . boxquote-describe-key)
+  ("<f12> b h w" . boxquote-where-is)
+  ("<f12> b !"   . boxquote-shell-command))
 (use-package cheat-sh
   :ensure t
   :bind
