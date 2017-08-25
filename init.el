aa7364017d748825bb454a82ca90e8d190a2e51c
Dave Pearson <davep@davep.org>
2017-08-25 11:07:44 +0100

    Tidy up the file.
    Those heavy section headers look too much when indented in the `let'.

--- init.el
+++ init.el
@@ -9,25 +9,20 @@
 ;; Don't let gc slow things down as we start up.
 (let ((gc-cons-threshold most-positive-fixnum))
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Add my local init directory to the load path.
   (push (expand-file-name "init.d/" user-emacs-directory) load-path)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Ensure local storage is defined and set up.
   (require 'init-local)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Ensure custom values go in their own file.
   (load (setq custom-file (local-emacs-directory "custom.el")) t)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Make sure the package system is up and running early on.
-  ;; (package-initialize) happens in here (and it's mentioned here to stop the
-  ;; package system stomping on my init file).
+  ;; (package-initialize) happens in here (and it's mentioned here to stop
+  ;; the package system stomping on my init file).
   (require 'init-repos)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Now that melpa is sorted and use-package is available, ensure that
   ;; auto-compile kicks in. I like my ~/.emacs.d/ to be compiled so this is
   ;; very handy when I've made lots of changes and pulled them down to a
@@ -38,11 +33,9 @@
     (auto-compile-on-save-mode)
     (auto-compile-on-load-mode))
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; is-a.el is a bit special, as I use very early on. Load it now.
   (use-package is-a :ensure t)
 
-  ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
   ;; Load various startup things.
   (unless noninteractive
     (require 'init-editing)
