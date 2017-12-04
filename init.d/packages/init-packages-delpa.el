6296287d573debfd28e00bbb88f5ea5010929645
Dave Pearson <davep@davep.org>
2017-12-04 15:28:55 +0000

    Don't nuke some Indium-related buffers

--- init.d/packages/init-packages-delpa.el
+++ init.d/packages/init-packages-delpa.el
@@ -65,6 +65,10 @@
   :ensure t)
 (use-package nuke-buffers
   :ensure t
+  :config
+  (add-to-list 'nuke-buffers-ignore "*node process*")
+  (add-to-list 'nuke-buffers-ignore "*JS scratch*")
+  (add-to-list 'nuke-buffers-ignore "*JS REPL*")
   :bind
   ("C-M-<f11>" . nuke-buffers))
 (use-package nukneval
