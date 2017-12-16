461e5f9a09cc3ecc3effedfcd24c0b17cc2e6bef
Dave Pearson <davep@davep.org>
2017-12-16 19:39:48 +0000

    Use festival.el

--- init.d/packages/init-packages-delpa.el
+++ init.d/packages/init-packages-delpa.el
@@ -26,6 +26,12 @@
   :ensure t
   :bind
   ("C-c C-e" . expando-macro))
+(when is-a-macOS-p
+  (use-package festival
+    :ensure t
+    :config
+    (setq festival-program "/usr/local/festival/festival/bin/festival")
+    (setq festival-auto-start t)))
 (use-package fscroll
   :ensure t)
 (use-package graburl
