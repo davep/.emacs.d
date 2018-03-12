57010c0590f105bc4ddb0bdf68c9033c60d0777b
Dave Pearson <davep@davep.org>
2018-03-12 16:49:51 +0000

    Start using dna-mode

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -97,6 +97,8 @@
   (add-hook 'after-init-hook #'global-company-mode))
 (use-package dictionary
   :ensure t)
+(use-package dna-mode
+  :ensure t)
 (when is-a-unix-p
   (use-package emojify
     :if is-a-unix-window-p
