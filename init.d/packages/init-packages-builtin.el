830a871db673467866b9eb9dfeec8fb46aee056e
Dave Pearson <davep@davep.org>
2018-04-24 09:53:47 +0100

    :sparkles: Add support for loading local org-mode config files
    This means I can do special things at work, and not have it bleed into home,
    etc (and not have to hold it as part of my general Emacs config in GitHub).

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -135,7 +135,8 @@
         org-default-notes-file (concat (file-name-as-directory org-directory) "inbox.org")
         org-agenda-files (list org-default-notes-file)
         org-log-done 'time
-        org-src-fontify-natively t))
+        org-src-fontify-natively t)
+  (mapcar #'load (directory-files "~/.config/org/" t "\\.el$")))
 (use-package quickurl
   :bind
   ("<f12> u u" . quickurl)
