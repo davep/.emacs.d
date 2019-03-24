3599d303f6371017ba88ac4b4b787ce281b95558
Dave Pearson <davep@davep.org>
2019-03-24 12:52:30 +0000

    :sparkles: Start using eshell-git-prompt
    https://github.com/xuchunyang/eshell-git-prompt

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -119,6 +119,18 @@
     :ensure t))
 (use-package dockerfile-mode
   :ensure t)
+(use-package eshell-git-prompt
+  :ensure t
+  :custom-face
+  (eshell-git-prompt-powerline-dir-face
+   ((t :background "grey80")))
+  (eshell-git-prompt-powerline-clean-face
+   ((t :background "green")))
+  (eshell-git-prompt-powerline-not-clean-face
+   ((t :background "yellow")))
+  :commands eshell-git-prompt-use-theme
+  :init
+  (eshell-git-prompt-use-theme 'powerline))
 (use-package highlight-chars
   :ensure t
   :demand
