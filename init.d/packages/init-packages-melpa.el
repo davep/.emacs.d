4ea0b9618007b458e3fe0a70aede46e9674d9645
Dave Pearson <dave.pearson@synpromics.com>
2019-07-29 13:41:17 +0100

    :sparkles: Ensure projectile always uses ivy

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -350,6 +350,7 @@
   (projectile-known-projects-file (local-emacs-directory "projectile-bookmarks.eld"))
   (projectile-mode-line-prefix " P")
   (projectile-switch-project-action 'neotree-projectile-action)
+  (projectile-completion-system 'ivy)
   :config
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map)
   (projectile-mode 1))
