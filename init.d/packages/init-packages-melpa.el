ed2aba8ebdb4b2b92526e0e7641771ee6a940ec6
Dave Pearson <dave.pearson@synpromics.com>
2019-08-12 09:44:02 +0100

    :sparkles: Stop projectile dropping its cache file in my config

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -348,6 +348,7 @@
   :ensure t
   :custom
   (projectile-known-projects-file (local-emacs-directory "projectile-bookmarks.eld"))
+  (projectile-cache-file (local-emacs-directory "projectile.cache"))
   (projectile-mode-line-prefix " P")
   (projectile-switch-project-action 'neotree-projectile-action)
   (projectile-completion-system 'ivy)
