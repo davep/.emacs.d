b3185a1dbaa0a16f540465844b84c5e1c6a77180
Dave Pearson <dave.pearson@synpromics.com>
2019-11-26 10:29:12 +0000

    :sparkles: Add org-present
    https://github.com/rlister/org-present

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -323,6 +323,25 @@
   :ensure t
   :after org
   :hook (org-mode . org-bullets-mode))
+(use-package org-present
+  :ensure t
+  :after org
+  :init
+  (progn
+  (add-hook 'org-present-mode-hook
+            (lambda ()
+              (org-present-big)
+              (org-display-inline-images)
+              (org-present-hide-cursor)
+              (org-present-read-only)
+              (display-line-numbers-mode 0)))
+  (add-hook 'org-present-mode-quit-hook
+            (lambda ()
+              (org-present-small)
+              (org-remove-inline-images)
+              (org-present-show-cursor)
+              (org-present-read-write)
+              (display-line-numbers-mode 1)))))
 (use-package package-lint
   :ensure t)
 (use-package page-break-lines
