08d3c14d9ae5fc3c1d351eb7ac2400f78fcbaff9
Dave Pearson <dave.pearson@synpromics.com>
2020-01-22 10:20:06 +0000

    :sparkles: Try and get pipenv-mode to be smarter

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -387,6 +387,8 @@
   :ensure t
   :diminish "PE"
   :hook (python-mode . pipenv-mode)
+  :init
+  (setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended)
   :bind
   ("<f12> p a" . pipenv-activate)
   ("<f12> p d" . pipenv-deactivate))
