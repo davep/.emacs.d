806bfa1f79cb164b6eb95d9b9b214aa20749e4e6
Dave Pearson <dave.pearson@synpromics.com>
2020-03-04 16:58:53 +0000

    :sparkles: Exclude files in Emacs' config from the recentf list

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -231,6 +231,8 @@
   :commands recentf-save-list
   :custom
   (recentf-save-file (local-emacs-directory "recentf"))
+  :config
+  (add-to-list 'recentf-exclude (rx ".local/share/emacs/"))
   :init
   ;; Auto-save the list every 30 minutes.
   (run-at-time nil (* 30 60)
