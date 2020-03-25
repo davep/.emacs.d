7050a06c5a1d33d89f3f46e432e3d32abdd5f370
Dave Pearson <dave.pearson@synpromics.com>
2020-03-25 17:27:54 +0000

    :fire: Don't mess with GC on startup
    While it seemed like a good idea, and it's something many do, I really don't
    start Emacs often enough to care if GC robs me of a second or so of startup
    time. I start Emacs once and run it for days, sometimes weeks.

--- init.el
+++ init.el
@@ -6,50 +6,47 @@
 
 ;;; Code:
 
-;; Don't let gc slow things down as we start up.
-(let ((gc-cons-threshold most-positive-fixnum))
+;; Add my local init directory to the load path.
+(push (expand-file-name "init.d/" user-emacs-directory) load-path)
 
-  ;; Add my local init directory to the load path.
-  (push (expand-file-name "init.d/" user-emacs-directory) load-path)
+;; Ensure local storage is defined and set up.
+(require 'init-local)
 
-  ;; Ensure local storage is defined and set up.
-  (require 'init-local)
+;; Ensure custom values go in their own file.
+(load (setq custom-file (local-emacs-directory "custom.el")) t)
 
-  ;; Ensure custom values go in their own file.
-  (load (setq custom-file (local-emacs-directory "custom.el")) t)
+;; Make sure the package system is up and running early on.
+;; (package-initialize) happens in here (and it's mentioned here to stop
+;; the package system stomping on my init file).
+(require 'init-repos)
 
-  ;; Make sure the package system is up and running early on.
-  ;; (package-initialize) happens in here (and it's mentioned here to stop
-  ;; the package system stomping on my init file).
-  (require 'init-repos)
+;; Now that the repos are set up, ensure `use-package' is in play.
+(require 'use-package)
 
-  ;; Now that the repos are set up, ensure `use-package' is in play.
-  (require 'use-package)
+;; Now that melpa is sorted and use-package is available, ensure that
+;; auto-compile kicks in. I like my ~/.emacs.d/ to be compiled so this is
+;; very handy when I've made lots of changes and pulled them down to a
+;; machine that had pre-existing elc files that will now be out of date.
+(use-package auto-compile
+  :ensure t
+  :config
+  (auto-compile-on-save-mode)
+  (auto-compile-on-load-mode))
 
-  ;; Now that melpa is sorted and use-package is available, ensure that
-  ;; auto-compile kicks in. I like my ~/.emacs.d/ to be compiled so this is
-  ;; very handy when I've made lots of changes and pulled them down to a
-  ;; machine that had pre-existing elc files that will now be out of date.
-  (use-package auto-compile
-    :ensure t
-    :config
-    (auto-compile-on-save-mode)
-    (auto-compile-on-load-mode))
+;; is-a.el is a bit special, as I use very early on. Load it now.
+(use-package is-a :ensure t)
 
-  ;; is-a.el is a bit special, as I use very early on. Load it now.
-  (use-package is-a :ensure t)
+;; Load various startup things.
+(unless noninteractive
+  (require 'init-editing)
+  (require 'init-tweaks)
+  (require 'init-keys)
+  (require 'init-style))
+(require 'init-net)
+(require 'init-packages)
 
-  ;; Load various startup things.
-  (unless noninteractive
-    (require 'init-editing)
-    (require 'init-tweaks)
-    (require 'init-keys)
-    (require 'init-style))
-  (require 'init-net)
-  (require 'init-packages)
-
-  ;; Finally, load any local config. This is for things I don't want to be
-  ;; tracking via my ~/.init.d repo code (things local to a machine, etc).
-  (load (local-emacs-directory "local-init.el") t))
+;; Finally, load any local config. This is for things I don't want to be
+;; tracking via my ~/.init.d repo code (things local to a machine, etc).
+(load (local-emacs-directory "local-init.el") t)
 
 ;;; init.el ends here
