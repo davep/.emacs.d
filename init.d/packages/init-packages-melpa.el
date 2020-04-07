53cc9d8efa92b621392a9ff889871a5bcbb8f14d
Dave Pearson <dave.pearson@synpromics.com>
2020-04-07 10:58:53 +0100

    :card_index: Use Roswell as my "Lisp"
    While I tend to still use sbcl as the Common Lisp of choice on my Macs,
    these days I tend to run things via Roswell.

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -455,7 +455,7 @@
 (use-package slime
   :ensure t
   :config
-  (setq inferior-lisp-program "sbcl")
+  (setq inferior-lisp-program "ros run")
   (add-hook 'slime-inferior-process-start-hook
             (lambda ()
               (require 'slime-fancy))))
