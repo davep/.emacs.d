f298fd8fd9226fdadf900d168e34220717c2fda9
Dave Pearson <dave.pearson@synpromics.com>
2020-04-14 14:09:05 +0100

    :hammer: First step: move one of my own packages into its own loader
    Here I'm picking one of my own packages, which is loaded from "delpa", and
    putting the loading of it into its own file. Baby steps to see if I like
    this idea and see if it makes sense.

--- init.d/packages/init-packages-delpa.el
+++ init.d/packages/init-packages-delpa.el
@@ -8,14 +8,6 @@
 
 ;;; Code:
 
-(use-package become
-  :ensure t
-  :commands become-free-of-trailing-whitespace
-  :init
-  (unless noninteractive
-    (add-hook 'before-save-hook #'become-free-of-trailing-whitespace))
-  :bind
-  ("<f12> <tab>" . become-freshly-indented-no-tabs))
 (use-package commoji
   :ensure t
   :bind
