0b77bac3d5fe3de010e17552a1d5135a438e1c18
Dave Pearson <dave.pearson@synpromics.com>
2020-04-15 13:44:04 +0100

    :hammer: Move winsplit.el into its own loader

--- init.d/packages/init-packages-delpa.el
+++ init.d/packages/init-packages-delpa.el
@@ -8,17 +8,6 @@
 
 ;;; Code:
 
-(use-package winsplit
-  :ensure t
-  :bind*
-  ("C-c <right>"   . winsplit-right)
-  ("C-c <left>"    . winsplit-left)
-  ("C-c <up>"      . winsplit-above)
-  ("C-c <down>"    . winsplit-below)
-  ("C-c C-<right>" . winsplit-right-load)
-  ("C-c C-<left>"  . winsplit-left-load)
-  ("C-c C-<up>"    . winsplit-above-load)
-  ("C-c C-<down>"  . winsplit-below-load))
 
 (provide 'init-packages-delpa)
 
---
+++ init.d/packages/use/delpa/winsplit.el
@@ -0,0 +1,11 @@
+(use-package winsplit
+  :ensure t
+  :bind*
+  ("C-c <right>"   . winsplit-right)
+  ("C-c <left>"    . winsplit-left)
+  ("C-c <up>"      . winsplit-above)
+  ("C-c <down>"    . winsplit-below)
+  ("C-c C-<right>" . winsplit-right-load)
+  ("C-c C-<left>"  . winsplit-left-load)
+  ("C-c C-<up>"    . winsplit-above-load)
+  ("C-c C-<down>"  . winsplit-below-load))
