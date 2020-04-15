d7c5d78ee6ccf0dd48c05b8e0ea61fe123920fe9
Dave Pearson <dave.pearson@synpromics.com>
2020-04-15 14:09:28 +0100

    :hammer: Move thinks.el into its own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -12,11 +12,6 @@
 
 
 ;; My own packages that are in melpa.
-(use-package thinks
-  :ensure t
-  :bind
-  ("<f12> t t" . thinks-maybe-region)
-  ("<f12> t y" . thinks-yank))
 (use-package uptimes
   :ensure t
   :custom
---
+++ init.d/packages/use/melpa/davep/thinks.el
@@ -0,0 +1,5 @@
+(use-package thinks
+  :ensure t
+  :bind
+  ("<f12> t t" . thinks-maybe-region)
+  ("<f12> t y" . thinks-yank))
