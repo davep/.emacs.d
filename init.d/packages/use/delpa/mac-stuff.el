53b17a23d3347bd569eea8039665646c68dfbef1
Dave Pearson <dave.pearson@synpromics.com>
2020-04-15 10:57:32 +0100

    :fire: Remove all the header/footer stuff
    I don't think it makes sense to treat these files as full-on packages that
    should pass linting and stuff. They're really just config files, that's the
    way to view it. Make them small and simple and easy to maintain.

--- init.d/packages/use/delpa/mac-stuff.el
+++ init.d/packages/use/delpa/mac-stuff.el
@@ -1,25 +1,3 @@
-;;; mac-stuff.el --- Loader for some Mac-specific things -*- lexical-binding: t -*-
-;; Copyright 2020 by Dave Pearson <davep@davep.org>
-
-;; This program is free software: you can redistribute it and/or modify it
-;; under the terms of the GNU General Public License as published by the
-;; Free Software Foundation, either version 3 of the License, or (at your
-;; option) any later version.
-;;
-;; This program is distributed in the hope that it will be useful, but
-;; WITHOUT ANY WARRANTY; without even the implied warranty of
-;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-;; Public License for more details.
-;;
-;; You should have received a copy of the GNU General Public License along
-;; with this program. If not, see <http://www.gnu.org/licenses/>.
-
-;;; Commentary:
-;;
-;; mac-stuff.el loads up some packages of mine that are specific to macOS.
-
-;;; Code:
-
 ;; Only bother with this if we're on macOS.
 (when is-a-macOS-p
 
@@ -28,5 +6,3 @@
 
   ;; Tool for getting some information about a Mac.
   (use-package macinfo :ensure t))
-
-;;; mac-stuff.el ends here
