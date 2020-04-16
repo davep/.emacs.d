e9129d269a285161432d009137ef68acbb298a47
Dave Pearson <dave.pearson@synpromics.com>
2020-04-16 12:24:31 +0100

    :hammer: Move magit and friends into their own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -12,33 +12,6 @@
 
 
 ;; Third-party packages from elpa/melpa
-(when is-a-unix-p
-  (use-package magit
-    :ensure t
-    :commands magit-after-save-refresh-status
-    :config
-    (add-hook 'after-save-hook #'magit-after-save-refresh-status)
-    (add-hook 'git-commit-setup-hook (lambda() (flyspell-mode 1)))
-    (setq ghub-use-workaround-for-emacs-bug 'force)
-    :bind
-    ("<f12> g s"   . magit-status)
-    ("<f12> g b"   . magit-blame)
-    ("<f12> g l a" . magit-log-all)
-    ("<f12> g l f" . magit-log-buffer-file))
-  (use-package forge
-    :ensure t
-    :custom
-    (forge-database-file (local-emacs-directory "forge-database.sqlite"))
-    :config
-    (push (list "gitlab.synpromics.com"
-                "gitlab.synpromics.com/api/v4"
-                "gitlab.synpromics.com"
-                'forge-gitlab-repository)
-          forge-alist))
-  (use-package transient
-    :ensure t
-    :custom
-    (transient-history-file (local-emacs-directory "transient-history.el"))))
 (use-package markdown-mode
   :ensure t
   :config
---
+++ init.d/packages/use/melpa/magit.el
@@ -0,0 +1,33 @@
+(require 'is-a)
+(require 'init-local)
+
+(when is-a-unix-p
+
+  (use-package magit
+    :ensure t
+    :commands magit-after-save-refresh-status
+    :config
+    (add-hook 'after-save-hook #'magit-after-save-refresh-status)
+    (add-hook 'git-commit-setup-hook (lambda() (flyspell-mode 1)))
+    (setq ghub-use-workaround-for-emacs-bug 'force)
+    :bind
+    ("<f12> g s"   . magit-status)
+    ("<f12> g b"   . magit-blame)
+    ("<f12> g l a" . magit-log-all)
+    ("<f12> g l f" . magit-log-buffer-file))
+
+  (use-package forge
+    :ensure t
+    :custom
+    (forge-database-file (local-emacs-directory "forge-database.sqlite"))
+    :config
+    (push (list "gitlab.synpromics.com"
+                "gitlab.synpromics.com/api/v4"
+                "gitlab.synpromics.com"
+                'forge-gitlab-repository)
+          forge-alist))
+
+  (use-package transient
+    :ensure t
+    :custom
+    (transient-history-file (local-emacs-directory "transient-history.el"))))
