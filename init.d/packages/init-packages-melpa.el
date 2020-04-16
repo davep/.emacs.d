8ef456c9b48d3acee7b8bc8f712124d800ddbaab
Dave Pearson <dave.pearson@synpromics.com>
2020-04-16 10:12:00 +0100

    :hammer: Move all-the-icons into its own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -12,19 +12,6 @@
 
 
 ;; Third-party packages from elpa/melpa
-(use-package all-the-icons
-  :ensure t)
-(use-package all-the-icons-dired
-  :ensure t
-  :after all-the-icons
-  :hook (dired-mode . all-the-icons-dired-mode))
-(use-package all-the-icons-ibuffer
-  :ensure t
-  :after all-the-icons
-  :init (all-the-icons-ibuffer-mode 1))
-(use-package all-the-icons-ivy-rich
-  :ensure t
-  :init (all-the-icons-ivy-rich-mode 1))
 (use-package apache-mode
   :ensure t)
 (use-package beacon
---
+++ init.d/packages/use/melpa/all-the-icons.el
@@ -0,0 +1,15 @@
+(use-package all-the-icons :ensure t)
+
+(use-package all-the-icons-dired
+  :ensure t
+  :after all-the-icons
+  :hook (dired-mode . all-the-icons-dired-mode))
+
+(use-package all-the-icons-ibuffer
+  :ensure t
+  :after all-the-icons
+  :init (all-the-icons-ibuffer-mode 1))
+
+(use-package all-the-icons-ivy-rich
+  :ensure t
+  :init (all-the-icons-ivy-rich-mode 1))
