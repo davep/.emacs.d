6cc631950fb97eafac4f81ee8ea255603b9bc408
Dave Pearson <dave.pearson@synpromics.com>
2020-04-16 12:21:30 +0100

    :hammer: Move 3rd party Org things into their own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -67,38 +67,6 @@
   :custom
   (neo-theme 'icons)
   (neo-window-width 30))
-(use-package org-bullets
-  :ensure t
-  :after org
-  :hook (org-mode . org-bullets-mode))
-(use-package org-present
-  :ensure t
-  :after org
-  :commands
-  org-present-big
-  org-present-hide-cursor
-  org-present-read-only
-  org-present-read-write
-  org-present-small
-  org-present-show-cursor
-  org-display-inline-images
-  org-remove-inline-images
-  :init
-  (progn
-  (add-hook 'org-present-mode-hook
-            (lambda ()
-              (org-present-big)
-              (org-display-inline-images)
-              (org-present-hide-cursor)
-              (org-present-read-only)
-              (display-line-numbers-mode 0)))
-  (add-hook 'org-present-mode-quit-hook
-            (lambda ()
-              (org-present-small)
-              (org-remove-inline-images)
-              (org-present-show-cursor)
-              (org-present-read-write)
-              (display-line-numbers-mode 1)))))
 (use-package package-lint
   :ensure t)
 (use-package page-break-lines
