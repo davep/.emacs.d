ba338f329b1fec0df18aef5fe2731293eb9005d3
Dave Pearson <dave.pearson@synpromics.com>
2020-04-16 11:35:05 +0100

    :hammer: Move dashboard into its own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -12,25 +12,6 @@
 
 
 ;; Third-party packages from elpa/melpa
-(use-package dashboard
-  :ensure t
-  :demand
-  :config
-  (setq dashboard-startup-banner 'logo
-        dashboard-set-footer     nil
-        dashboard-items          '((projects  . 10)
-                                   (recents   . 10)
-                                   (bookmarks . 10)))
-  (dashboard-setup-startup-hook)
-  :commands
-  dashboard-insert-startupify-lists
-  :bind
-  ("<f7>" . (lambda ()
-              (interactive)
-              (when (get-buffer dashboard-buffer-name)
-                (kill-buffer dashboard-buffer-name))
-              (dashboard-insert-startupify-lists)
-              (switch-to-buffer dashboard-buffer-name))))
 (use-package dictionary
   :ensure t
   :bind
