ad8b4a0b320c671d4881db866d609fdb0b2cccb7
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 13:35:48 +0100

    :hammer: Move powerline into its own loader

---
+++ init.d/packages/use/melpa/powerline.el
@@ -0,0 +1,14 @@
+(use-package powerline
+  :ensure t
+  :custom
+  (powerline-default-separator 'utf-8)
+  :config
+  (when is-a-macOS-p
+    (set (intern "ns-use-srgb-colorspace") nil))
+  (set-face-background 'powerline-active1 "grey95")
+  (set-face-foreground 'powerline-active1 "grey50")
+  (set-face-background 'powerline-active2 "grey85")
+  (set-face-foreground 'powerline-active2 "grey50")
+  (set-face-background 'powerline-inactive1 "grey95")
+  (set-face-background 'powerline-inactive2 "grey85")
+  (powerline-default-theme))
