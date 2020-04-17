43f9d634c0c99ec9d11c1f16a1b1838c07bf2bb7
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 10:38:54 +0100

    :sparkles: Experiment with having moving.el take over C-a/e
    For a long time, I've used moving.el by having the commands bound to Home
    and End, but I've always kept C-a and C-e as the Emacs defaults. I'm
    thinking it might be time to experiment with taking them over too.
    
    I can't see an obvious downside at the moment.

--- init.d/packages/use/delpa/moving.el
+++ init.d/packages/use/delpa/moving.el
@@ -1,8 +1,10 @@
 (use-package moving
   :ensure t
   :bind
+  ("C-a"       . moving-home)
   ("<home>"    . moving-home)
   ("s-<left>"  . moving-home)
+  ("C-e"       . moving-end)
   ("<end>"     . moving-end)
   ("s-<right>" . moving-end)
   ("s-<up>"    . moving-backward-page)
