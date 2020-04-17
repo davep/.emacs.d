2c0d6369d628cca05e117f2e3626f2705662581e
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 13:35:16 +0100

    :hammer: Move pipenv into its own loader

--- init.d/packages/init-packages-melpa.el
+++ init.d/packages/init-packages-melpa.el
@@ -12,16 +12,6 @@
 
 
 ;; Third-party packages from elpa/melpa
-(use-package pipenv
-  :ensure t
-  :diminish "PE"
-  :hook (python-mode . pipenv-mode)
-  :commands pipenv-projectile-after-switch-extended
-  :init
-  (setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended)
-  :bind
-  ("<f12> p a" . pipenv-activate)
-  ("<f12> p d" . pipenv-deactivate))
 (use-package powerline
   :ensure t
   :custom
