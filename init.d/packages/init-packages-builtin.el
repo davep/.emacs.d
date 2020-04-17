8a5719462f7ac513dc700cdea8d3012650a31643
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 15:05:19 +0100

    :hammer: Move elec-pair config into its own file

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -14,27 +14,6 @@
 
 
 ;; General modes.
-(use-package elec-pair
-  :commands electric-pair-mode
-  :init
-  (electric-pair-mode t)
-  :custom
-  (electric-pair-inhibit-predicate
-   (lambda (c)
-     (or
-      ;; This isn't ideal. I often don't want pair completion inside the
-      ;; minibuffer, but there are times when I might (`eval-expression'
-      ;; being one such time when I'd want it). Given the former is more
-      ;; common than the latter, and I can't really see a good way of only
-      ;; turning this off for some things, let's turn it off any time we're
-      ;; in the minibuffer.
-      (eq major-mode 'minibuffer-inactive-mode)
-      ;; Don't complete { when in web-mode and using the Django engine, as
-      ;; it does its own thing.
-      (and
-       (eq major-mode 'web-mode)
-       (string= (symbol-value 'web-mode-engine) "django")
-       (= c ?{))))))
 (use-package eshell
   :defines eshell-visual-commands
   :functions eshell/pwd
