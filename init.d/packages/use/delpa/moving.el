8c4ebd7ef65a47fccf0a0168bb9b4e4322da6c12
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 13:51:46 +0100

    :fire: Remove the remapping of C-a and C-e
    My muscle memory hates 43f9d634c0c99ec9d11c1f16a1b1838c07bf2bb7

--- init.d/packages/use/delpa/moving.el
+++ init.d/packages/use/delpa/moving.el
@@ -1,10 +1,8 @@
 (use-package moving
   :ensure t
   :bind
-  ("C-a"       . moving-home)
   ("<home>"    . moving-home)
   ("s-<left>"  . moving-home)
-  ("C-e"       . moving-end)
   ("<end>"     . moving-end)
   ("s-<right>" . moving-end)
   ("s-<up>"    . moving-backward-page)
