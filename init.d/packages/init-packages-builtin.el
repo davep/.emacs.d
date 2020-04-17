c4c2df28b7062c3e3cf26926d98037f5cd7de8ce
Dave Pearson <dave.pearson@synpromics.com>
2020-04-17 14:58:07 +0100

    :hammer: Move dired config into its own file

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -14,18 +14,6 @@
 
 
 ;; General modes.
-(use-package dired
-  :config
-  (setq dired-use-ls-dired nil))
-(use-package dired-x
-  :config
-  (setq-default dired-omit-files-p t)
-  (setq dired-omit-files
-        (rx (or
-             ;; Any file whose name starts with a "."
-             (group bol "." (not (any ".")))
-             ;; Any sort of Python cache.
-             "__pycache__"))))
 (use-package eldoc
   :diminish)
 (use-package elec-pair
---
+++ init.d/packages/use/builtin/dired.el
@@ -0,0 +1,13 @@
+(use-package dired
+  :config
+  (setq dired-use-ls-dired nil))
+
+(use-package dired-x
+  :config
+  (setq-default dired-omit-files-p t)
+  (setq dired-omit-files
+        (rx (or
+             ;; Any file whose name starts with a "."
+             (group bol "." (not (any ".")))
+             ;; Any sort of Python cache.
+             "__pycache__"))))
