aa8bfc6add43226a15d468fbd250c600e4f56177
Dave Pearson <dave.pearson@synpromics.com>
2020-04-20 13:38:06 +0100

    :hammer: Move message config into its own file

---
+++ init.d/packages/use/builtin/message.el
@@ -0,0 +1,9 @@
+(use-package message
+  :config
+  (add-hook 'message-mode-hook
+            (lambda ()
+              (setq smartsig-set-signature #'msig-set
+                    smartsig-abbrev-table  'message-mode-abbrev-table)
+              (smartsig-clear)
+              (smartsig-add "emacs"   "~/.sigs/emacs"   "emacs" "xemacs" "elisp" "gnu" "lbdb" "uptimes" "quickurl" "smartsig" "boxquote")
+              (smartsig-add "sawfish" "~/.sigs/sawfish" "sawfish" "sawmill" "librep" "rep" "gnome"))))
