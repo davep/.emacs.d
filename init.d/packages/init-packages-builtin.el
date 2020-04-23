607a372bb9644099dc5e9455232261185a7b5917
Dave Pearson <dave.pearson@synpromics.com>
2020-04-23 16:12:42 +0100

    :hammer: Move coding modes config into its own file

--- init.d/packages/init-packages-builtin.el
+++ init.d/packages/init-packages-builtin.el
@@ -13,39 +13,9 @@
 (require 'init-local)
 
 
-;; Programming modes.
-(use-package cc-mode
-  :custom
-  (c-basic-offset 4)
-  (c-offsets-alist '((inline-open . 0)
-                     (case-label . +)))
-  (c-default-style '((c-mode    . "BSD")
-                     (c++-mode  . "BSD")
-                     (java-mode . "java")
-                     (awk-mode  . "awk")
-                     (other     . "gnu")))
-  :bind
-  (:map c-mode-map   ("RET" . newline-and-indent))
-  (:map c++-mode-map ("RET" . newline-and-indent)))
-(use-package js2-mode
-  :mode "\\.js$"
-  :custom
-  (js-switch-indent-offset 4))
-(use-package python
-  :custom
-  (python-indent-guess-indent-offset nil)
-  (python-shell-interpreter "python3")
-  (python-fill-docstring-style 'pep-257-nn))
-
-
 ;; Catch-all hooks, etc.
 (add-hook 'text-mode-hook
           (lambda ()
-            (auto-fill-mode 1)))
-(add-hook 'prog-mode-hook
-          (lambda ()
-            ;; Set up for auto-filling comments.
-            (setq-local comment-auto-fill-only-comments t)
             (auto-fill-mode 1)))
 
 (provide 'init-packages-builtin)
