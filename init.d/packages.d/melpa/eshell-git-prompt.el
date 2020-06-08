b530834039195c0327bbca86aa3cca2bdd57d28f
Dave Pearson <dave.pearson@synpromics.com>
2020-06-08 14:03:43 +0100

    :lipstick: Make the eshell powerline config dark-mode-friendly
    This might nobble things for light mode. I'll see when I next drop into it.

--- init.d/packages.d/melpa/eshell-git-prompt.el
+++ init.d/packages.d/melpa/eshell-git-prompt.el
@@ -2,11 +2,11 @@
   :ensure t
   :custom-face
   (eshell-git-prompt-powerline-dir-face
-   ((t :background "grey80")))
+   ((t :background "#3a3a3a" :foregorund "white")))
   (eshell-git-prompt-powerline-clean-face
-   ((t :background "green")))
+   ((t :background "green" :foreground "#005f00")))
   (eshell-git-prompt-powerline-not-clean-face
-   ((t :background "yellow")))
+   ((t :background "#d7af5f" :foreground "#626262")))
   :commands eshell-git-prompt-use-theme
   :init
   (eshell-git-prompt-use-theme 'powerline))
