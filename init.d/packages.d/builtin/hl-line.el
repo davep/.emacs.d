f8389ac8f93b66c614ef82705929b86f01efc130
Dave Pearson <dave.pearson@synpromics.com>
2020-06-10 15:07:08 +0100

    :fire: Drop using hl-line
    Sure, it's a nice idea, but I'm finding that it's throwing me off-by-one
    when highlighting lines for killing, etc.

--- init.d/packages.d/builtin/hl-line.el
+++
@@ -1,9 +0,0 @@
-(use-package hl-line
-  ;; Turn on for every buffer.
-  :init (global-hl-line-mode 1)
-  :custom
-  ;; Keep the highlight visible in every buffer
-  (global-hl-line-sticky-flag t)
-  :config
-  ;; Turn off for eshell though 'cos it messes with the prompt.
-  (add-hook 'eshell-mode-hook (lambda () (setq-local global-hl-line-mode nil))))
