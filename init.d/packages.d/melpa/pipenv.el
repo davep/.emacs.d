e0f67430d2c2a44c1f73e60ab0f5ab57403e251b
Dave Pearson <dave.pearson@synpromics.com>
2020-08-17 09:24:41 +0100

    :fire: pipenv from Homebrew is behaving again
    I can now safely remove the environment variable that turns off all warnings
    and the like, which was throwing off the Emacs pipenv package.

--- init.d/packages.d/melpa/pipenv.el
+++ init.d/packages.d/melpa/pipenv.el
@@ -5,9 +5,6 @@
   :commands pipenv-projectile-after-switch-extended
   :init
   (setq pipenv-projectile-after-switch-function #'pipenv-projectile-after-switch-extended)
-  ;; Pipenv via Homebrew is having some issues at the moment, which throws
-  ;; off this code. So let's work around that.
-  (setenv "PIPENV_VERBOSITY" "-1")
   :bind
   ("<f12> p a" . pipenv-activate)
   ("<f12> p d" . pipenv-deactivate))
