42fc345272642b92849e328769544cac8ee21130
Dave Pearson <dave.pearson@synpromics.com>
2020-08-20 14:24:19 +0100

    :card_index: Admit defeat and use pydocstyle for now
    I can't seem to convince lsp-mode to use pylint and nothing else for all the
    linting stuff, so go with pydocstyle as the in-editor nagging system. Given
    that the way pydocstyle lets you configure it with a file (which is really
    unhelpful -- every name and location seems to be designed to be unhelpful!)
    let's just configure it right inside Emacs itself.
    
    At least this way I have my preferences stored in a location where they will
    get replicated and I can document them.

--- init.d/packages.d/melpa/lsp.el
+++ init.d/packages.d/melpa/lsp.el
@@ -5,12 +5,30 @@
   :hook
   (python-mode . lsp)
   :custom
+  ;; The following is an attempt to try and convince lsp-mode and/or pyls to
+  ;; use pylint and nothing else to lint my code. Right now it doesn't see
+  ;; to be making much difference. Despite everything else being disabled it
+  ;; seems to still be falling back to using pydocstyle. :-/
   (lsp-pyls-configuration-sources ["pylint"])
   (lsp-pyls-plugins-pydocstyle-enabled nil)
   (lsp-pyls-plugins-pyflakes-enabled nil)
   (lsp-pyls-plugins-flake8-enabled nil)
   (lsp-pyls-plugins-pylint-enabled t)
+  ;; Because of the above, try and tweak pydocstyle to use the style I
+  ;; prefer for all my code. Mostly this is about having slightly longer
+  ;; lines and not being so damn upset about whitespace.
+  (lsp-pyls-plugins-pycodestyle-max-line-length 120) ; This isn't a punched card.
+  (lsp-pyls-plugins-pycodestyle-ignore
+   ["E221" "E251" "E272"                ; I like to line things up.
+    "E201" "E202"                       ; I like space inside parens.
+    "E302"                              ; I don't need double vertical spacing.
+    "E266"                              ; Emacs-like end of file markers is
+                                        ; a hill I will die on.
+    ])
+  ;; Don't dump files in my Emacs config directory.
   (lsp-session-file (local-emacs-directory ".lsp-session-v1"))
+  ;; Because I don't have yasnippet set up and lsp-mode seems to be getting
+  ;; upset about that.
   (lsp-enable-snippet nil))
 
 (use-package lsp-ui
