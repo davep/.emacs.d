b19b62579b9f5d629d20cd436e6445ac2b946bb6
Dave Pearson <dave.pearson@synpromics.com>
2021-12-07 14:20:49 +0000

    :sparkles: Try and improve in my chosen mc bindings
    There are lots of mc/ commands that I keep forgetting about, and which would
    be really handy. Mostly I think I forget about them because they're on
    chords that I can't recall. So here I'm coming up with something that, to me
    now, feels like it makes more sense. Let's see if this sticks.

--- init.d/packages.d/melpa/mc.el
+++ init.d/packages.d/melpa/mc.el
@@ -1,8 +1,8 @@
 (use-package multiple-cursors
   :ensure t
   :bind
-  ("C-."     . mc/mark-next-like-this)
-  ("C-,"     . mc/mark-previous-like-this)
-  ("C->"     . mc/mark-all-like-this)
-  ("C-c ."   . mc/mark-all-like-this-dwim)
+  ("s-."     . mc/mark-next-like-this)
+  ("s-,"     . mc/mark-previous-like-this)
+  ("s-*"     . mc/mark-all-like-this)
+  ("s-@"     . mc/mark-all-like-this-dwim)
   ("C-c C-." . mc/edit-lines))
