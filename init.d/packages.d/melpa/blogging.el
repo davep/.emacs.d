7f72a729873c9b634de5711ce4acc9342d0643aa
Dave Pearson <davep@davep.org>
2022-06-01 23:07:29 +0100

    :sparkles: Bind easy-jekyll to a key chord
    (P)ost (B)log.

--- init.d/packages.d/melpa/blogging.el
+++ init.d/packages.d/melpa/blogging.el
@@ -1,3 +1,4 @@
 (use-package easy-jekyll
   :ensure t
-  :custom (easy-jekyll-basedir "~/write/davep.github.com"))
+  :custom (easy-jekyll-basedir "~/write/davep.github.com")
+  :bind ("<f12> p b" . easy-jekyll))
