c6c9c0dd118773c3bac865c8e05ac3cf8ec57165
Dave Pearson <dpearson@askbio.com>
2022-08-22 11:28:39 +0100

    :hammer: Switch to using pyright
    It's been a while since I set up lsp stuff in Emacs, and also since I got it
    working for Python, and it seems the language server I'd gone with has been
    deprecated for quite some time. Finally trying to switch away from it.
    
    For now I'm keeping all my old settings here, commented out (yes, I know,
    comments as version control), until I'm happy with the new setup. As for the
    new setup: I'm trying out pyright.

--- init.d/packages.d/melpa/lsp.el
+++ init.d/packages.d/melpa/lsp.el
@@ -1,47 +1,58 @@
 ;; <URL:https://emacs-lsp.github.io/lsp-mode/page/installation/#use-package>
 (use-package lsp-mode
   :ensure t
-  :commands lsp
+  :commands lsp)
+
+(use-package lsp-pyright
+  :ensure t
   :hook
-  (python-mode . lsp)
+  (python-mode . (lambda ()
+                   (require 'lsp-pyright)
+                   (lsp)))
   :custom
-  ;; The following is an attempt to try and convince lsp-mode and/or pyls to
-  ;; use pylint and nothing else to lint my code. Right now it doesn't seem
-  ;; to be making much difference. Despite everything else being disabled it
-  ;; seems to still be falling back to using pydocstyle. :-/
-  (lsp-pyls-configuration-sources ["pylint"])
-  (lsp-pyls-plugins-pydocstyle-enabled nil)
-  (lsp-pyls-plugins-pyflakes-enabled nil)
-  (lsp-pyls-plugins-flake8-enabled nil)
-  (lsp-pyls-plugins-pylint-enabled t)
-  ;; Because of the above, try and tweak pydocstyle to use the style I
-  ;; prefer for all my code. Mostly this is about having slightly longer
-  ;; lines and not being so damn upset about whitespace.
-  (lsp-pyls-plugins-pycodestyle-max-line-length 120) ; This isn't a punched card.
-  (lsp-pyls-plugins-pycodestyle-ignore
-   [
-    ;; I like to line things up.
-    "E221" "E241" "E251" "E271" "E272"
-    ;; I like space inside parens.
-    "E201" "E202"
-    ;; Sometimes just one space before a comment.
-    "E261"
-    ; I don't need double vertical spacing.
-    "E302" "E305"
-    ;; I don't like space around parameter equals.
-    "E252"
-    ;; Emacs-like end of file markers is a hill I will die on.
-    "E266"
-    ;; W503 and W504 are kind of mutually exclusive, and looking at the docs
-    ;; (https://www.flake8rules.com/rules/W503.html) W503 is deprecated
-    ;; anyway. So let's ignore W503.
-    "W503"
-    ])
   ;; Don't dump files in my Emacs config directory.
-  (lsp-session-file (local-emacs-directory ".lsp-session-v1"))
-  ;; Because I don't have yasnippet set up and lsp-mode seems to be getting
-  ;; upset about that.
-  (lsp-enable-snippet nil))
+  (lsp-session-file (local-emacs-directory ".lsp-session-v1")))
+
+;; :hook
+;; (python-mode . lsp)
+;; :custom
+;; ;; The following is an attempt to try and convince lsp-mode and/or pyls to
+;; ;; use pylint and nothing else to lint my code. Right now it doesn't seem
+;; ;; to be making much difference. Despite everything else being disabled it
+;; ;; seems to still be falling back to using pydocstyle. :-/
+;; (lsp-pyls-configuration-sources ["pylint"])
+;; (lsp-pyls-plugins-pydocstyle-enabled nil)
+;; (lsp-pyls-plugins-pyflakes-enabled nil)
+;; (lsp-pyls-plugins-flake8-enabled nil)
+;; (lsp-pyls-plugins-pylint-enabled t)
+;; ;; Because of the above, try and tweak pydocstyle to use the style I
+;; ;; prefer for all my code. Mostly this is about having slightly longer
+;; ;; lines and not being so damn upset about whitespace.
+;; (lsp-pyls-plugins-pycodestyle-max-line-length 120) ; This isn't a punched card.
+;; (lsp-pyls-plugins-pycodestyle-ignore
+;;  [
+;;   ;; I like to line things up.
+;;   "E221" "E241" "E251" "E271" "E272"
+;;   ;; I like space inside parens.
+;;   "E201" "E202"
+;;   ;; Sometimes just one space before a comment.
+;;   "E261"
+;;   ; I don't need double vertical spacing.
+;;   "E302" "E305"
+;;   ;; I don't like space around parameter equals.
+;;   "E252"
+;;   ;; Emacs-like end of file markers is a hill I will die on.
+;;   "E266"
+;;   ;; W503 and W504 are kind of mutually exclusive, and looking at the docs
+;;   ;; (https://www.flake8rules.com/rules/W503.html) W503 is deprecated
+;;   ;; anyway. So let's ignore W503.
+;;   "W503"
+;;   ])
+;; ;; Don't dump files in my Emacs config directory.
+;; (lsp-session-file (local-emacs-directory ".lsp-session-v1"))
+;; ;; Because I don't have yasnippet set up and lsp-mode seems to be getting
+;; ;; upset about that.
+;; (lsp-enable-snippet nil))
 
 (use-package lsp-ui
   :ensure t
