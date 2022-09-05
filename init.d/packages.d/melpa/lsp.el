487f210b7c1eed27447b1a20ed81582fb27a5044
Dave Pearson <dpearson@askbio.com>
2022-09-05 11:09:46 +0100

    :fire: Don't delay loading lso-pyright
    It seemed to make sense to declare it that way but all it's doing is causing
    things to not get going.

--- init.d/packages.d/melpa/lsp.el
+++ init.d/packages.d/melpa/lsp.el
@@ -6,7 +6,6 @@
 ;; <URL:https://emacs-lsp.github.io/lsp-pyright/>
 (use-package lsp-pyright
   :ensure t
-  :after lsp-mode
   :hook
   (python-mode . (lambda ()
                    (require 'lsp-pyright)
