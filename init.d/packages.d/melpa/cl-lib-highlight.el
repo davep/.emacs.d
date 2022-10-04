1f8fbd5f1717f2af1e532ef8134d0b8d55b43a70
Dave Pearson <davep@davep.org>
2022-10-04 22:10:57 +0100

    :heavy_minus_sign: Remove cl-lib-highlight
    It's not been necessary for a while now and it also seems to have been
    removed from melpa.

--- init.d/packages.d/melpa/cl-lib-highlight.el
+++
@@ -1,8 +0,0 @@
-(use-package cl-lib-highlight
-  :ensure t
-  :commands
-  cl-lib-highlight-initialize
-  cl-lib-highlight-warn-cl-initialize
-  :init
-  (cl-lib-highlight-initialize)
-  (cl-lib-highlight-warn-cl-initialize))
