3f24bdc13ab08eea1ab00900f41370cf7a71bd80
Dave Pearson <davep@davep.org>
2022-10-22 21:34:57 +0100

    :sparkles: Try and improve Python vent management
    The pipenv and poetry packages haven't been doing a great job for me, and
    auto-venv looks like it might be just the ticket. So... let's go all-in on
    this. Removing the pipenv and poetry packages and going to try and lean
    fully on auto-venv switching.

---
+++ init.d/packages.d/melpa/auto-venv.el
@@ -0,0 +1,9 @@
+(use-package pyvenv :ensure t)
+
+(use-package auto-virtualenv
+  :ensure t
+  :config
+  (add-hook 'window-configuration-change-hook 'auto-virtualenv-set-virtualenv)
+  (add-hook 'focus-in-hook 'auto-virtualenv-set-virtualenv)
+  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
+  (add-hook ' projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv))
--- init.d/packages.d/melpa/poetry.el
+++
@@ -1,8 +0,0 @@
-(use-package poetry
-  :ensure t
-  :commands poetry
-  :init
-  (poetry-tracking-mode)
-  :bind
-  ("<f12> p o". poetry))
-
