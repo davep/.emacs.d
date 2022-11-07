c2f788410ec9c88ff719ff1ee4a98e9dc8374855
Dave Pearson <davep@davep.org>
2022-11-07 16:55:34 +0000

    :pencil: Fix a typo

--- init.d/packages.d/melpa/auto-venv.el
+++ init.d/packages.d/melpa/auto-venv.el
@@ -6,4 +6,4 @@
   (add-hook 'window-configuration-change-hook 'auto-virtualenv-set-virtualenv)
   (add-hook 'focus-in-hook 'auto-virtualenv-set-virtualenv)
   (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
-  (add-hook ' projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv))
+  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv))
