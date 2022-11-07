97e62b74de86117e23e0e2beb603f5c07f980699
Dave Pearson <davep@davep.org>
2022-11-07 14:32:55 +0000

    :sparkles: Stop doing emoji rendering in conf and prog modes

--- init.d/packages.d/melpa/emojify.el
+++ init.d/packages.d/melpa/emojify.el
@@ -5,6 +5,8 @@
     :config
     (setq emojify-emojis-dir (local-emacs-directory "emojis"))
     (add-to-list 'emojify-inhibit-major-modes 'restclient-mode)
+    (add-to-list 'emojify-inhibit-major-modes 'prog-mode)
+    (add-to-list 'emojify-inhibit-major-modes 'conf-mode)
     :init
     (global-emojify-mode)
     :ensure t))
