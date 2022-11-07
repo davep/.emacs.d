49e5dfc2a3db920c70cd339f07fdcc74eff5b9f3
Dave Pearson <davep@davep.org>
2022-11-07 10:03:36 +0000

    :sparkles: Use aspell everywhere

--- init.d/packages.d/builtin/ispell.el
+++ init.d/packages.d/builtin/ispell.el
@@ -4,12 +4,7 @@
   :custom
   (ispell-dictionary     "british")
   (ispell-highlight-face 'flyspell-incorrect)
-  :config
-  ;; Try and get aspell working on a Windows machine.
-  (let ((aspell "C:/Program Files (x86)/Aspell/bin/"))
-    (when (and is-a-win32-p (file-exists-p aspell))
-      (push aspell exec-path)
-      (setq ispell-program-name "aspell"))))
+  (ispell-program-name   "aspell"))
 
 (use-package flyspell
   :diminish
