523351d78089f1f2e2c0403bcaa915e67fda580a
Dave Pearson <davep@davep.org>
2022-12-28 21:18:47 +0000

    :pencil: Fix a typo
    Goodness knows how/when this appeared -- yasnippet has been working just fine.

--- init.d/packages.d/melpa/yasnippet.el
+++ init.d/packages.d/melpa/yasnippet.el
@@ -1,7 +1,7 @@
 (use-package yasnippet
   :ensure t
   :custom
-  (yas-snipper-dirs (list (locate-user-emacs-file ".snippets")))
+  (yas-snippet-dirs (list (locate-user-emacs-file ".snippets")))
   :config
   (yas-global-mode 1))
 
