12669ac128a2677b780bbc931fa0e7b31a3f9550
Dave Pearson <davep@davep.org>
2023-01-23 09:28:54 +0000

    :lipstick: I don't need to advertise in the modeline

--- init.d/packages.d/melpa/yasnippet.el
+++ init.d/packages.d/melpa/yasnippet.el
@@ -1,5 +1,6 @@
 (use-package yasnippet
   :ensure t
+  :diminish yas-minor-mode
   :custom
   (yas-snippet-dirs (list (locate-user-emacs-file ".snippets")))
   :hook (eglot-managed-mode . (lambda () (setq-local company-backends '((company-capf :with company-yasnippet)))))
