6dbca7d8e9ec4f829010143629ae54c2a91793e9
Dave Pearson <davep@davep.org>
2023-01-23 09:32:28 +0000

    :fire: I don't ever really use deft

--- init.d/packages.d/melpa/deft.el
+++
@@ -1,9 +0,0 @@
-(use-package deft
-  :ensure t
-  :custom
-  (deft-directory "~/notebook")
-  (deft-recursive t)
-  (deft-use-filename-as-title t)
-  :commands deft
-  :bind
-  ("<f12> RET" . deft))
