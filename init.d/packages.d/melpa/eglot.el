571d3b26e1d01cd0f4877e89f75a7d945690d084
Dave Pearson <davep@davep.org>
2023-03-19 09:00:10 +0000

    :sparkles: Add a binding for eldoc
    Done as part of the eglot config, which it's not really, but that's how I'm
    going to be thinking about it.

--- init.d/packages.d/melpa/eglot.el
+++ init.d/packages.d/melpa/eglot.el
@@ -4,6 +4,9 @@
   :after company
   :hook (python-mode . eglot-ensure)
   :bind
+  ;; Some of these obviously aren't actual eglot things, but they're things
+  ;; that I find most useful when working via eglot.
+  ("<f12> e d" . eldoc)
   ("<f12> e r" . eglot-rename)
   ("<f12> e f d" . eglot-find-declaration)
   ("<f12> e f i" . eglot-find-implementation)
