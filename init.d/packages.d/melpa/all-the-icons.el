26b246dbdf3d3644d5721cba73d7fbe83d7f6032
Dave Pearson <davep@davep.org>
2023-04-16 18:19:29 +0100

    :construction: Workaround for a formatting issue in all-the-icons-ivy-rich

--- init.d/packages.d/melpa/all-the-icons.el
+++ init.d/packages.d/melpa/all-the-icons.el
@@ -10,7 +10,11 @@
 (use-package all-the-icons-ibuffer
   :ensure t
   :after all-the-icons
-  :init (all-the-icons-ibuffer-mode 1))
+  :init (all-the-icons-ibuffer-mode 1)
+  ;; Quick fix for:
+  ;; https://github.com/seagle0128/all-the-icons-ivy-rich/issues/33
+  ;; Once that's sorted, kill this off.
+  :custom (all-the-icons-ivy-rich-icon-size 0.95))
 
 (use-package all-the-icons-ivy-rich
   :ensure t
