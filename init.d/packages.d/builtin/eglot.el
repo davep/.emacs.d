a7c106b54d04779de73ae7c483b2ea4d02d72f8b
Dave Pearson <davep@davep.org>
2023-09-14 08:12:27 +0100

    :sparkles: Add a binding for eglot-reconnect
    I often find that if I update anything with my Python venvs under the hood,
    eglot can get a little confused about what's going on (which is fair); so
    often I need to reconnect. Might as well give this a binding.

--- init.d/packages.d/builtin/eglot.el
+++ init.d/packages.d/builtin/eglot.el
@@ -10,6 +10,7 @@
   ;; that I find most useful when working via eglot.
   ("<f12> e d" . eldoc)
   ("<f12> e r" . eglot-rename)
+  ("<f12> e c" . eglot-reconnect)
   ("<f12> e f d" . eglot-find-declaration)
   ("<f12> e f i" . eglot-find-implementation)
   ("<f12> e f t" . eglot-find-typeDefinition)
