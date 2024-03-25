2637adfcfa05ebcc30275c6afeca428ad4a82b25
Dave Pearson <davep@davep.org>
2024-03-25 20:13:22 +0000

    :sparkles: Add some bindings for mode-on-region

--- init.d/packages.d/melpa/mode-on-region.el
+++ init.d/packages.d/melpa/mode-on-region.el
@@ -1,3 +1,9 @@
 ;;; https://github.com/quelpa/quelpa-use-package
 (use-package mode-on-region
-  :quelpa (mode-on-region :fetcher github :repo "miketz/mor"))
+  :quelpa (mode-on-region :fetcher github :repo "miketz/mor")
+  :bind
+  ("<f12> RET" . mor-mode-on-region)
+  ("<f12> C-<return>" . mor-prev-mode-on-region)
+  (:map mor-tmp-buffer-mode)
+  ("C-c C-c" . mor-copy-back)
+  ("C-c C-x" . mor-close-tmp-buffer))
