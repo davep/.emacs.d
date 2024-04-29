1100d39aede810446ac412b927c499698c08b6c4
Dave Pearson <davep@davep.org>
2024-04-29 09:28:37 +0100

    :sparkles: Add git gutter

---
+++ init.d/packages.d/melpa/git-gutter.el
@@ -0,0 +1,14 @@
+;; https://ianyepan.github.io/posts/emacs-git-gutter/
+
+(use-package git-gutter
+  :ensure t
+  :hook (prog-mode . git-gutter-mode)
+  :config
+  (setq git-gutter:update-interval 0.02))
+
+(use-package git-gutter-fringe
+  :ensure t
+  :config
+  (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
+  (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
