4acddf9e550418293636d898680774084756ff4f
Dave Pearson <davep@davep.org>
2024-09-12 09:09:30 +0100

    :zap: Tidy up the configuration of dictionary.el

--- init.d/packages.d/melpa/dictionary.el
+++ init.d/packages.d/melpa/dictionary.el
@@ -1,4 +1,7 @@
 (use-package dictionary
   :ensure t
   :bind
-  ("<f12> f d" . dictionary-lookup-definition))
+  ("<f12> f d" . dictionary-lookup-definition)
+  ("<f12> / d" . dictionary-search)
+  :custom
+  (dictionary-server "dict.org"))
