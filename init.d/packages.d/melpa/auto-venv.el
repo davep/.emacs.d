4c340c2da90a5486b987f4ec39413592baa89224
Dave Pearson <davep@davep.org>
2024-11-04 17:29:05 +0000

    :hammer: Adapt to the changed auto-virtualenv
    I don't think this is the full change I need to make, but at least this
    stops Emacs from crashing.

--- init.d/packages.d/melpa/auto-venv.el
+++ init.d/packages.d/melpa/auto-venv.el
@@ -3,7 +3,7 @@
 (use-package auto-virtualenv
   :ensure t
   :config
-  (add-hook 'window-configuration-change-hook 'auto-virtualenv-set-virtualenv)
-  (add-hook 'focus-in-hook 'auto-virtualenv-set-virtualenv)
-  (add-hook 'python-mode-hook 'auto-virtualenv-set-virtualenv)
-  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-set-virtualenv))
+  (add-hook 'window-configuration-change-hook 'auto-virtualenv-find-and-activate)
+  (add-hook 'focus-in-hook 'auto-virtualenv-find-and-activate)
+  (add-hook 'python-mode-hook 'auto-virtualenv-find-and-activate)
+  (add-hook 'projectile-after-switch-project-hook 'auto-virtualenv-find-and-activate))
