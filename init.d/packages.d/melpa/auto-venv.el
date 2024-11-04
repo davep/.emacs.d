f009bccd1a72ad20125f49d3e0bc3f67dcd1171e
Dave Pearson <davep@davep.org>
2024-11-04 17:34:26 +0000

    :sparkles: Make auto-virtualenv less verbose

--- init.d/packages.d/melpa/auto-venv.el
+++ init.d/packages.d/melpa/auto-venv.el
@@ -3,6 +3,7 @@
 (use-package auto-virtualenv
   :ensure t
   :config
+  (setq auto-virtualenv-verbose nil)
   (add-hook 'window-configuration-change-hook 'auto-virtualenv-find-and-activate)
   (add-hook 'focus-in-hook 'auto-virtualenv-find-and-activate)
   (add-hook 'python-mode-hook 'auto-virtualenv-find-and-activate)
