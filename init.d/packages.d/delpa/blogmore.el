68bfb0cc0e6113f7aaa333f884915e3a08ebe650
Dave Pearson <davep@davep.org>
2026-03-20 16:24:39 +0000

    :sparkles: Use a hook for end-it on new blog posts

--- init.d/packages.d/delpa/blogmore.el
+++ init.d/packages.d/delpa/blogmore.el
@@ -1,2 +1,3 @@
 (use-package blogmore
-  :vc (:url "https://github.com/davep/blogmore.el" :rev :newest))
+  :vc (:url "https://github.com/davep/blogmore.el" :rev :newest)
+  :init (add-hook 'blogmore-new-post-hook #'end-it))
