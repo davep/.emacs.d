ca36eef3e30736772fc570cd5cbe010c1aca69d5
Dave Pearson <davep@davep.org>
2026-03-25 15:04:32 +0000

    :sparkles: Add keyboard bindings for BlogMore commands

--- init.d/packages.d/delpa/blogmore.el
+++ init.d/packages.d/delpa/blogmore.el
@@ -1,3 +1,13 @@
 (use-package blogmore
   :vc (:url "https://github.com/davep/blogmore.el" :rev :newest)
-  :init (add-hook 'blogmore-new-post-hook #'end-it))
+  :init (add-hook 'blogmore-new-post-hook #'end-it)
+  :bind
+  ("<f12> m p n" . blogmore-new)
+  ("<f12> m p e" . blogmore-edit)
+  ("<f12> m s c" . blogmore-set-category)
+  ("<f12> m a t" . blogmore-add-tag)
+  ("<f12> m u d" . blogmore-update-date)
+  ("<f12> m u m" . blogmore-update-modified)
+  ("<f12> m l p" . blogmore-link-post)
+  ("<f12> m l c" . blogmore-link-category)
+  ("<f12> m l t" . blogmore-link-tag))
