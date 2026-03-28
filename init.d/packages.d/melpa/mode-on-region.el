5e40dc965d18a20800b5fcd5f88a95c6728d1727
Dave Pearson <davep@davep.org>
2026-03-28 11:13:21 +0000

    :fire: Remove mode-on-region

--- init.d/packages.d/melpa/mode-on-region.el
+++
@@ -1,9 +0,0 @@
-;;; https://github.com/quelpa/quelpa-use-package
-(use-package mode-on-region
-  :quelpa (mode-on-region :fetcher github :repo "davep/mor")
-  :bind
-  ("<f12> RET" . mor-mode-on-region)
-  ("<f12> C-<return>" . mor-prev-mode-on-region)
-  (:map mor-tmp-buffer-mode)
-  ("C-c C-c" . mor-copy-back)
-  ("C-c C-x" . mor-close-tmp-buffer))
