788d9352e699597282af1c22da001af5f3a1a30b
Dave Pearson <davep@davep.org>
2026-03-30 10:38:30 +0100

    :sparkles: Add lexically.el

---
+++ init.d/packages.d/personal/lexically.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package lexically
+  :ensure t
+  :vc (:url "https://github.com/davep/lexically.el" :rev :newest)
+  :bind
+  ("<f12> e l" . lexically))
+
+;;; lexically.el ends here
