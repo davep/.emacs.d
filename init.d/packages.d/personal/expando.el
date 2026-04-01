6899749cad141b91aec9d5577a378647722c478a
Dave Pearson <davep@davep.org>
2026-04-01 09:10:54 +0100

    :heavy_plus_sign: Add expando back to my confg

---
+++ init.d/packages.d/personal/expando.el
@@ -0,0 +1,8 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package expando
+  :vc (:url "https://github.com/davep/expando.el" :rev :newest)
+  :bind
+  ("C-c e" . expando-macro))
+
+;;; expando.el ends here
