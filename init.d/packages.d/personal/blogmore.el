2de5f055c6d3aa92f50f78a0776ee63b3eec5e98
Dave Pearson <davep@davep.org>
2026-04-04 11:12:58 +0100

    :zap: Simplify bindings for blogmore.el
    Make use of the transient.

--- init.d/packages.d/personal/blogmore.el
+++ init.d/packages.d/personal/blogmore.el
@@ -12,16 +12,6 @@
    '(("blog.davep.org" "~/write/davep.github.com/content/posts/")
      ("seen-by.davep.dev" "~/write/seen-by/content/posts/")))
   :bind
-  ("<f12> b b" . blogmore-work-on)
-  ("<f12> b n" . blogmore-new)
-  ("<f12> b e" . blogmore-edit)
-  ("<f12> b d" . blogmore-toggle-draft)
-  ("<f12> b s c" . blogmore-set-category)
-  ("<f12> b a t" . blogmore-add-tag)
-  ("<f12> b u d" . blogmore-update-date)
-  ("<f12> b u m" . blogmore-update-modified)
-  ("<f12> b l p" . blogmore-link-post)
-  ("<f12> b l c" . blogmore-link-category)
-  ("<f12> b l t" . blogmore-link-tag))
+  ("<f12> b" . blogmore))
 
 ;;; blogmore.el ends here
