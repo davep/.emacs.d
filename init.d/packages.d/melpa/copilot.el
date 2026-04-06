1b26ac94d0329d0ace8261d3b15a0e52dbb459cf
Dave Pearson <davep@davep.org>
2026-04-06 09:34:17 +0100

    :art: Clean up the declaration for copilot

--- init.d/packages.d/melpa/copilot.el
+++ init.d/packages.d/melpa/copilot.el
@@ -2,12 +2,11 @@
 
 (use-package copilot
   :ensure t
+  :defer t
   :hook (prog-mode . copilot-mode)
   :bind (:map copilot-completion-map
               ("<tab>" . copilot-accept-completion)
-              ("TAB" . copilot-accept-completion)
               ("C-<tab>" . copilot-accept-completion-by-word)
-              ("C-TAB" . copilot-accept-completion-by-word)
               ("C-n" . copilot-next-completion)
               ("C-p" . copilot-previous-completion))
   :custom
