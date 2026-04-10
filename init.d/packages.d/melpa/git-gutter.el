499a1327d95a10a4c33f0b7d775d376e52c9b16f
Dave Pearson <davep@davep.org>
2026-04-10 11:03:19 +0100

    :card_index: Diminish git-gutter

--- init.d/packages.d/melpa/git-gutter.el
+++ init.d/packages.d/melpa/git-gutter.el
@@ -1,7 +1,8 @@
-;; https://ianyepan.github.io/posts/emacs-git-gutter/
+;; https://ianyepan.github.io/posts/emacs-git-gutter/  -*- lexical-binding: t; -*-
 
 (use-package git-gutter
   :ensure t
+  :diminish
   :hook (prog-mode . git-gutter-mode)
   :config
   (setq git-gutter:update-interval 0.02))
@@ -12,3 +13,5 @@
   (define-fringe-bitmap 'git-gutter-fr:added [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:modified [224] nil nil '(center repeated))
   (define-fringe-bitmap 'git-gutter-fr:deleted [128 192 224 240] nil nil 'bottom))
+
+;;; git-gutter.el ends here
