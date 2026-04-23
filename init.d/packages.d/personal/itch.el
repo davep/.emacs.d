0e66b25e5518553846f851c7c72bc83bd83402e9
Dave Pearson <davep@davep.org>
2026-04-23 21:19:34 +0100

    :zap: Add a binding for itch-markdown-scratch-buffer

--- init.d/packages.d/personal/itch.el
+++ init.d/packages.d/personal/itch.el
@@ -5,7 +5,7 @@
   :defer t
   :vc (:url "https://github.com/davep/itch.el" :rev :newest)
   :bind*
-  ("M-s"         . itch-scratch-buffer)
-  ("<XF86Eject>" . itch-scratch-buffer))
+  ("M-s" . itch-scratch-buffer)
+  ("M-s-s" . itch-markdown-scratch-buffer))
 
 ;;; itch.el ends here
