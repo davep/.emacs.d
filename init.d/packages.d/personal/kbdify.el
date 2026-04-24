69a15ec843cbe9b22a442d838243b5e90ff5acef
Dave Pearson <davep@davep.org>
2026-04-24 15:31:03 +0100

    :sparkles: Add kbdify

---
+++ init.d/packages.d/personal/kbdify.el
@@ -0,0 +1,10 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package kbdify
+  :ensure t
+  :defer t
+  :vc (:url "https://github.com/davep/kbdify.el")
+  :bind
+  ("<f12> k" . kbdify))
+
+;;; kbdify.el ends here
