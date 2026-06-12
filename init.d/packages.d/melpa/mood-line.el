ebab9264c05663624a4e7ca172a2e836e05141b6
Dave Pearson <davep@davep.org>
2026-06-12 20:22:28 +0100

    :lipstick: Further simplify the mode line

--- init.d/packages.d/melpa/mood-line.el
+++ init.d/packages.d/melpa/mood-line.el
@@ -7,11 +7,14 @@
   (setq mood-line-format
         (mood-line-defformat
          :left
-         (((mood-line-segment-buffer-status) . " ")
-          ((mood-line-segment-buffer-name) . " : ")
+         ((mood-line-segment-buffer-name)
+          " "
+          (or (mood-line-segment-buffer-status) ":")
+          " "
           (mood-line-segment-major-mode))
          :right
-         (((mood-line-segment-vc) . "  ")
+         ((mood-line-segment-vc)
+          " "
           (mood-line-segment-cursor-position))))
   (mood-line-mode 1))
 
