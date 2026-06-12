d04acbf2d65268f5aef5310aacd219e02b899aec
Dave Pearson <davep@davep.org>
2026-06-12 18:56:03 +0100

    :lipstick: Revamp my modeline
    Drop powerline (thank you, you served me well) and use mood-line with
    a *very* minimal setup.

---
+++ init.d/packages.d/melpa/mood-line.el
@@ -0,0 +1,18 @@
+;; -*- lexical-binding: t; -*-
+
+(use-package mood-line
+  :ensure t
+  :config
+  (setq mood-line-glyph-alist mood-line-glyphs-fira-code)
+  (setq mood-line-format
+        (mood-line-defformat
+         :left
+         (((mood-line-segment-buffer-status) . " ")
+          ((mood-line-segment-buffer-name) . " : ")
+          (mood-line-segment-major-mode))
+         :right
+         (((mood-line-segment-vc) . "  ")
+          (mood-line-segment-cursor-position))))
+  (mood-line-mode 1))
+
+;;; mood-line.el ends here
