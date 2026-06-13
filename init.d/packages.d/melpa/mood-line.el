eb6c0a8263573e03ae1ffe0a52d57cbcd8623980
Dave Pearson <davep@davep.org>
2026-06-13 09:46:57 +0100

    :lipstick: Make the status indicator more interesting

--- init.d/packages.d/melpa/mood-line.el
+++ init.d/packages.d/melpa/mood-line.el
@@ -3,14 +3,22 @@
 (use-package mood-line
   :ensure t
   :config
+  (defface my/mood-line-good-status
+    '((t (:foreground "#009900" :weight bold)))
+    "Face for good status in mood-line."
+    :group 'mood-line)
   (setq mood-line-glyph-alist mood-line-glyphs-fira-code)
   (setq mood-line-format
         (mood-line-defformat
          :left
-         ((mood-line-segment-buffer-name)
+         ((or
+           (mood-line-segment-buffer-status)
+           (propertize
+            (mood-line--get-glyph :buffer-modified)
+            'face 'my/mood-line-good-status))
           " "
-          (or (mood-line-segment-buffer-status) ":")
-          " "
+          (mood-line-segment-buffer-name)
+          " : "
           (mood-line-segment-major-mode))
          :right
          ((mood-line-segment-vc)
