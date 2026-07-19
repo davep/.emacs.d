5de4cb5a83aab59e21cce6f9d172d5bd208e6ee1
Dave Pearson <davep@davep.org>
2026-07-19 20:21:23 +0100

    :zap: Improve the Gemtext configuration

--- init.d/packages.d/melpa/gemtext.el
+++ init.d/packages.d/melpa/gemtext.el
@@ -2,6 +2,12 @@
 
 (use-package gemtext-mode
   :ensure t
-  :mode ("\\.gmi\\'" . gemtext-mode))
+  :mode ("\\.gmi\\'" . gemtext-mode)
+  :hook
+  (gemtext-mode . (lambda ()
+                    (auto-fill-mode -1)
+                    (visual-line-mode 1)
+                    (setq-local fill-column 120)
+                    (visual-fill-column-mode 1))))
 
 ;;; gemtext.el ends here
