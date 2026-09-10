dc102609c1fe3840c1dcf2ccf62ec7288fb88a2b
Dave Pearson <davep@davep.org>
2026-09-10 08:53:10 +0100

    :sparkles: Stop ielm from littering my ~/.emacs.d directory

---
+++ init.d/packages.d/builtin/ielm.el
@@ -0,0 +1,9 @@
+;; -*- lexical-binding: t; -*-
+
+(require 'init-local)
+
+(use-package ielm
+  :custom
+  (ielm-history-file-name (local-emacs-directory "ielm-history.eld")))
+
+;;; ielm.el ends here
