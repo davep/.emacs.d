80be8e27f574650985624e6d90bd4bdb8d8864ac
Dave Pearson <davep@davep.org>
2026-09-10 19:09:47 +0100

    :art: Fix tramp littering my ~/.emacs.d directory

--- init.d/packages.d/builtin/tramp.el
+++ init.d/packages.d/builtin/tramp.el
@@ -1,8 +1,9 @@
 ;; -*- lexical-binding: t; -*-
 
 (use-package tramp
+  :init
+  (setq tramp-persistency-file-name (local-emacs-directory "tramp.el"))
   :custom
-  (tramp-default-method        "ssh")
-  (tramp-persistency-file-name (local-emacs-directory "tramp.el")))
+  (tramp-default-method "ssh"))
 
 ;;; tramp.el ends here
